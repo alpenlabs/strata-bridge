a043ef60978e0de7d0fbcb95b83302c87fd60eab
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-01-28 15:28:12 +0545

    feat(test-utils): add ArbitraryGenerator

--- crates/test-utils/src/lib.rs
+++ crates/test-utils/src/lib.rs
@@ -4,6 +4,7 @@
 //! implementing `Arbitrary` is not feasible due to the orphan rule (without using newtypes for
 //! everything).
 
+pub mod arbitrary_generator;
 pub mod bitcoin;
 pub mod musig2;
 pub mod prelude;
