84063ec491e4d51524c50ee9833de0da65d45098
Jose Storopoli <jose@storopoli.com>
2025-01-29 16:08:30 -0300

    feat(stake-chain): add ΔS timelock

--- crates/stake-chain/src/stake_chain.rs
+++ crates/stake-chain/src/stake_chain.rs
@@ -1,7 +1,7 @@
 //! The stake chain is a series of transactions that move the stake from a previous stake to a new
 //! stake.
 
-use bitcoin::{Amount, TxOut};
+use bitcoin::{relative, Amount, TxOut};
 use serde::{Deserialize, Serialize};
 
 /// A [`StakeChain`] is a series of transactions that move the stake from a previous stake to a new
@@ -19,6 +19,9 @@ use serde::{Deserialize, Serialize};
 ///
 /// The funding UTXO is used to fund the stake and should cover not only for the stake transaction
 /// itself but also for all the dust amounts in the transaction graph.
+///
+/// The stake chain can be advanced forward by revealing a pre-image to a locking script that is
+/// also relative timelocked to a certain `ΔS` interval.
 ///
 /// The staking amount is the amount that is staked in the transaction graph for a single stake
 /// along with its dust amounts.
@@ -40,6 +43,9 @@ pub struct StakeChain {
 
     /// The staking amount.
     pub amount: Amount,
+
+    /// The `ΔS` interval relative timelock to advance the stake chain.
+    delta: relative::LockTime,
 
     /// Maximum number of slashing transactions to be created.
     pub max_slashing_transactions: u32,
