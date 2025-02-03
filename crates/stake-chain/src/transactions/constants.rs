349e830d79c84d141ef452fc32b8cca02f606b54
Jose Storopoli <jose@storopoli.com>
2025-02-03 11:52:07 -0300

    fix(stake-chain): 71 pairs of dust output for Assert-data

--- crates/stake-chain/src/transactions/constants.rs
+++ crates/stake-chain/src/transactions/constants.rs
@@ -9,13 +9,13 @@ use bitcoin::Amount;
 ///
 /// For each single [`StakeTx`](crate::transactions::StakeTx), the number of dust outputs is:
 ///
-/// - 79 pairs of dust outputs for the "Assert-data" transaction: `330 * 2 * 79 = 52_140` sats.
+/// - 71 pairs of dust outputs for the "Assert-data" transaction: `330 * 2 * 71 = 46_860` sats.
 /// - 1 pair of dust outputs for the "Claim" transaction: `330 * 2 = 660` sats.
 /// - 1 dust output for the CPFP in the "Pre-Assert" transaction: `330` sats.
 /// - 1 dust output for the CPFP for the "Claim" transaction: `330` sats.
 ///
-/// The total is: `52_140 + 660 + 330 + 330 = 53_460` sats.
-pub const OPERATOR_FUNDS: Amount = Amount::from_sat((330 * 2 * 79) + (330 * 2) + 330 + 330);
+/// The total is: `46_860 + 660 + 330 + 330 = 53_460` sats.
+pub const OPERATOR_FUNDS: Amount = Amount::from_sat((330 * 2 * 71) + (330 * 2) + 330 + 330);
 
 #[cfg(test)]
 mod tests {
@@ -23,6 +23,6 @@ mod tests {
 
     #[test]
     fn operator_funds() {
-        assert_eq!(OPERATOR_FUNDS, Amount::from_sat(53_460));
+        assert_eq!(OPERATOR_FUNDS, Amount::from_sat(48_180));
     }
 }
