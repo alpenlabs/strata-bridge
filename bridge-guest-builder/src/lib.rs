edd3036a4f9ff7b8293965178690a0ee590a02a7
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-02-06 17:09:24 +0545

    feat: return empty elf in lint

--- bridge-guest-builder/src/lib.rs
+++ bridge-guest-builder/src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(not(skip_guest_build))]
 use sp1_sdk::include_elf;
 
+#[cfg(skip_guest_build)]
+pub const GUEST_BRIDGE_ELF: &[u8] = &[];
+
+#[cfg(not(skip_guest_build))]
 pub const GUEST_BRIDGE_ELF: &[u8] = include_elf!("strata-bridge-guest");
