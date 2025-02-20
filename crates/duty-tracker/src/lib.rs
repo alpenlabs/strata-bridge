3c3f499782d222162451c4c08753046ef5e76718
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-02-20 13:29:15 -0700

    persistence wip

--- crates/duty-tracker/src/lib.rs
+++ crates/duty-tracker/src/lib.rs
@@ -1,5 +1,6 @@
 //! TODO(proofofkeags): crate level documentation
 #![feature(result_flattening)]
+pub mod contract_persister;
 pub mod contract_state_machine;
 pub mod predicates;
 pub mod tx_driver;
