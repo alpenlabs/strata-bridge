93d10bc701a395c3d9cc65d36aa5e3f46d5819e6
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-02-25 16:55:15 -0700

    wip contract manager

--- crates/duty-tracker/src/lib.rs
+++ crates/duty-tracker/src/lib.rs
@@ -1,5 +1,6 @@
 //! TODO(proofofkeags): crate level documentation
 #![feature(result_flattening)]
+pub mod contract_manager;
 pub mod contract_persister;
 pub mod contract_state_machine;
 pub mod predicates;
