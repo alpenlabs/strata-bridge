b7dbd63c395da7f293bc5605da163b6609e92941
Jose Storopoli <jose@storopoli.com>
2025-02-28 16:10:16 -0300

    chore: export some important stuff in top-level

--- crates/p2p-impl/src/lib.rs
+++ crates/p2p-impl/src/lib.rs
@@ -5,6 +5,10 @@ pub mod config;
 pub mod constants;
 pub mod message_handler;
 
+pub use bootstrap::bootstrap;
+pub use config::Configuration;
+pub use message_handler::MessageHandler;
+
 #[cfg(test)]
 mod tests {
     use std::sync::LazyLock;
