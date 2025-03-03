fdf5c8623f204959333062560742c1b447fb47b7
krsnapaudel <dilli@alpenlabs.io>
2025-03-03 16:56:48 +0545

    first draft of monitoring api

--- crates/rpc/bridge/src/lib.rs
+++ crates/rpc/bridge/src/lib.rs
@@ -4,7 +4,13 @@
 //! decomposed into various groups partly based on how bitcoin RPCs are categorized into various
 //! [groups](https://developer.bitcoin.org/reference/rpc/index.html).
 
+mod types;
+
+use bitcoin::{OutPoint, Txid};
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use strata_bridge_primitives::{duties::BridgeDuty, types::OperatorIdx};
+
+use crate::types::*;
 
 /// RPCs related to information about the client itself.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "stratabridge"))]
@@ -14,4 +20,40 @@ pub trait StrataBridgeControlApi {
     /// increasing.
     #[method(name = "uptime")]
     async fn get_uptime(&self) -> RpcResult<u64>;
+}
+
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "stratabridge"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "stratabridge"))]
+pub trait StrataBridgeMonitoringApi {
+    /// Get all bridge operator IDs
+    #[method(name = "bridgeOperators")]
+    async fn get_bridge_operators(&self) -> RpcResult<Vec<OperatorIdx>>;
+
+    /// Query operator status (Online/Offline)
+    #[method(name = "operatorStatus")]
+    async fn get_operator_status(&self, operator_idx: OperatorIdx) -> RpcResult<OperatorStatus>;
+
+    /// Get deposit details using the deposit request outpoint
+    #[method(name = "depositInfo")]
+    async fn get_deposit_info(
+        &self,
+        deposit_request_outpoint: OutPoint,
+    ) -> RpcResult<RpcDepositInfo>;
+
+    /// Get bridge duties assigned to an operator
+    #[method(name = "bridgeDuties")]
+    async fn get_bridge_duties(&self, operator_id: u32) -> RpcResult<Vec<BridgeDuty>>;
+
+    /// Get withdrawal details using deposit outpoint
+    #[method(name = "withdrawalInfo")]
+    async fn get_withdrawal_info(&self, deposit_outpoint: OutPoint)
+        -> RpcResult<RpcWithdrawalInfo>;
+
+    /// Get all claim transaction IDs
+    #[method(name = "claims")]
+    async fn get_claims(&self) -> RpcResult<Vec<Txid>>;
+
+    /// Get claim details for a given claim transaction ID
+    #[method(name = "claimInfo")]
+    async fn get_claim_info(&self, claim_txid: Txid) -> RpcResult<RpcClaimInfo>;
 }
---
+++ crates/rpc/bridge/src/types.rs
@@ -0,0 +1,63 @@
+use bitcoin::{OutPoint, Txid};
+use serde::{Deserialize, Serialize};
+
+/// Enum representing the status of a bridge operator
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum OperatorStatus {
+    Online,
+    Offline,
+}
+
+/// Transaction status variants with custom serialization
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum TransactionStatus {
+    #[serde(rename = "In progress")]
+    InProgress,
+
+    #[serde(rename = "Challenged")]
+    Challenged,
+
+    #[serde(rename = "Cancelled")]
+    Cancelled,
+
+    #[serde(rename = "Complete")]
+    Complete,
+}
+
+/// Represents deposit transaction details
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcDepositInfo {
+    pub deposit_request_txid: Txid,
+    pub deposit_txid: Option<Txid>, // Some deposits may not have completed yet
+    pub status: TransactionStatus,
+}
+
+/// Represents withdrawal transaction details
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcWithdrawalInfo {
+    // Some withdrawals may still be pending
+    pub fulfillment_txid: Option<Txid>,
+    pub status: TransactionStatus,
+}
+
+/// Challenge step states for claims
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum ChallengeStep {
+    Claim,
+    Challenge,
+    Assert,
+}
+
+/// Represents reimbursement claim details
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcClaimInfo {
+    pub claim_txid: Txid,
+    pub challenge_step: ChallengeStep, // Fixed typo from "challege_step"
+    pub payout_txid: Option<Txid>,     // Some claims may not have completed payout yet
+    pub status: TransactionStatus,
+}
