00fd1ba6e48852d666672a0c4494b82a37699c4d
krsnapaudel <dilli@alpenlabs.io>
2025-03-05 15:06:29 +0545

    consistently use snake_case; deposit status update

--- crates/rpc/bridge/src/types.rs
+++ crates/rpc/bridge/src/types.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 /// Enum representing the status of a bridge operator
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
+#[serde(rename_all = "snake_case")]
 pub enum RpcOperatorStatus {
     Online,
     Offline,
@@ -11,21 +11,18 @@ pub enum RpcOperatorStatus {
 
 /// Represents a valid deposit status
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "status")]
+#[serde(tag = "status", rename_all = "snake_case")]
 pub enum RpcDepositStatus {
     /// Deposit exists, but minting hasn't happened yet.
-    #[serde(rename = "In progress")]
-    InProgress {
-        deposit_request_txid: Txid,
-        deposit_txid: Option<Txid>,
-    },
+    InProgress { deposit_request_txid: Txid },
 
     /// Deposit exists, but was never completed (can be reclaimed).
-    #[serde(rename = "Failed")]
-    Failed { deposit_request_txid: Txid },
+    Failed {
+        deposit_request_txid: Txid,
+        failure_reason: String,
+    },
 
     /// Deposit has been fully processed and minted.
-    #[serde(rename = "Complete")]
     Complete {
         deposit_request_txid: Txid,
         deposit_txid: Txid,
@@ -34,38 +31,32 @@ pub enum RpcDepositStatus {
 
 /// Shared status and relevant info for withdrawals and claims
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "status")]
+#[serde(tag = "status", rename_all = "snake_case")]
 pub enum RpcWithdrawalStatus {
     /// Claim exists, no payout yet.
-    #[serde(rename = "In progress")]
     InProgress { claim_txid: Txid },
 
     /// Claim exists, challenge step is "Challenge" or "Assert", no payout.
-    #[serde(rename = "Challenged")]
     Challenged {
         claim_txid: Txid,
         challenge_step: ChallengeStep,
     },
 
     /// Operator was slashed, claim is no longer valid.
-    #[serde(rename = "Cancelled")]
     Cancelled { claim_txid: Txid },
 
     /// Claim has been successfully reimbursed.
-    #[serde(rename = "Complete")]
     Complete { claim_txid: Txid, payout_txid: Txid },
 }
 
 /// Represents deposit transaction details
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct RpcDepositInfo {
     pub status: RpcDepositStatus,
 }
 
 /// Represents withdrawal transaction details
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct RpcWithdrawalInfo {
     // Some withdrawals may still be pending
     pub fulfillment_txid: Option<Txid>,
@@ -74,7 +65,7 @@ pub struct RpcWithdrawalInfo {
 
 /// Challenge step states for claims
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
+#[serde(rename_all = "snake_case")]
 pub enum ChallengeStep {
     Claim,
     Challenge,
@@ -83,7 +74,6 @@ pub enum ChallengeStep {
 
 /// Represents reimbursement claim details
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct RpcClaimInfo {
     pub status: RpcWithdrawalStatus,
 }
