a604e8c978ab0a9aaa920d55c32b1e8826829306
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-03-27 18:19:33 +0800

    fixes

--- crates/agent/src/duty_watcher.rs
+++ crates/agent/src/duty_watcher.rs
@@ -1,23 +1,16 @@
 use std::{sync::Arc, time::Duration};
 
-use bitcoin::{Amount, OutPoint, TapNodeHash, Txid};
-use bitcoin_bosd::Descriptor;
-use serde::{Deserialize, Serialize};
+use bitcoin::Txid;
 use strata_bridge_db::tracker::DutyTrackerDb;
-use strata_bridge_primitives::{
-    bitcoin::BitcoinAddress,
-    deposit::DepositInfo,
-    duties::{BridgeDuty, BridgeDutyStatus},
-    types::{BitcoinBlockHeight, OperatorIdx},
-    withdrawal::WithdrawalInfo,
-};
+use strata_bridge_primitives::duties::{BridgeDuty, BridgeDutyStatus};
 use strata_rpc_api::StrataApiClient;
-use strata_rpc_types::RpcBridgeDuties;
 use tokio::{
     sync::{broadcast, mpsc},
     task::JoinSet,
 };
 use tracing::{debug, error, info};
+
+use crate::strata_rpc::{BridgeDutyInteropTraitClient, RpcBridgeDutiesInterop};
 
 #[derive(Debug, Clone)]
 pub struct DutyWatcherConfig {
@@ -25,7 +18,10 @@ pub struct DutyWatcherConfig {
 }
 
 #[derive(Debug, Clone)]
-pub struct DutyWatcher<StrataClient: StrataApiClient, Db: DutyTrackerDb> {
+pub struct DutyWatcher<
+    StrataClient: StrataApiClient + BridgeDutyInteropTraitClient,
+    Db: DutyTrackerDb,
+> {
     config: DutyWatcherConfig,
 
     strata_rpc_client: Arc<StrataClient>,
@@ -35,7 +31,7 @@ pub struct DutyWatcher<StrataClient: StrataApiClient, Db: DutyTrackerDb> {
 
 impl<StrataClient, Db> DutyWatcher<StrataClient, Db>
 where
-    StrataClient: StrataApiClient + Send + Sync + 'static,
+    StrataClient: StrataApiClient + BridgeDutyInteropTraitClient + Send + Sync + 'static,
     Db: DutyTrackerDb + Send + Sync + 'static,
 {
     pub fn new(
@@ -79,14 +75,12 @@ where
                 let operator_idx = u32::MAX; // doesn't really matter in the current impl
                 let last_fetched_duty_index = db.get_last_fetched_duty_index().await.unwrap(); // FIXME:
                                                                                                // Handle me
-                let mut stake_index = 0;
 
-                let mut stake_index = 0;
                 match strata_rpc_client
                     .get_bridge_duties(operator_idx, last_fetched_duty_index)
                     .await
                 {
-                    Ok(RpcBridgeDuties {
+                    Ok(RpcBridgeDutiesInterop {
                         duties,
                         start_index,
                         stop_index,
@@ -96,100 +90,14 @@ where
 
                         for duty in duties {
                             debug!(action = "dispatching duty", ?duty);
-                            #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-                            struct DepositInfoInterop {
-                                /// The deposit request transaction outpoints from the users.
-                                pub deposit_request_outpoint: OutPoint,
 
-                                /// The execution layer address to mint the equivalent tokens to.
-                                /// As of now, this is just the 20-byte EVM address.
-                                pub el_address: Vec<u8>,
-
-                                /// The amount in bitcoins that the user is sending.
-                                ///
-                                /// This amount should be greater than the [`BRIDGE_DENOMINATION`]
-                                /// for the deposit to be confirmed
-                                /// on bitcoin. The excess amount is used as miner fees for the
-                                /// Deposit Transaction.
-                                pub total_amount: Amount,
-
-                                /// The hash of the take back leaf in the Deposit Request
-                                /// Transaction (DRT) as provided by the
-                                /// user in their `OP_RETURN` output.
-                                pub take_back_leaf_hash: TapNodeHash,
-
-                                /// The original taproot address in the Deposit Request Transaction
-                                /// (DRT) output used to
-                                /// sanity check computation internally i.e., whether the known
-                                /// information (n/n script spend
-                                /// path, [`static@UNSPENDABLE_INTERNAL_KEY`]) + the
-                                /// [`Self::take_back_leaf_hash`] yields the
-                                /// same P2TR address.
-                                pub original_taproot_addr: BitcoinAddress,
-                            }
-
-                            #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-                            struct CooperativeWithdrawalInfoInterop {
-                                /// The [`OutPoint`] of the UTXO in the Bridge Address that is to
-                                /// be used to service the
-                                /// withdrawal request.
-                                pub deposit_outpoint: OutPoint,
-
-                                /// The BOSD [`Descriptor`] supplied by the user.
-                                pub user_destination: Descriptor,
-
-                                /// The index of the operator that is assigned the withdrawal.
-                                pub assigned_operator_idx: OperatorIdx,
-
-                                /// The bitcoin block height before which the withdrawal has to be
-                                /// processed.
-                                ///
-                                /// Any withdrawal request whose `exec_deadline` is before the
-                                /// current bitcoin block height is
-                                /// considered stale and must be ignored.
-                                pub exec_deadline: BitcoinBlockHeight,
-                            }
-
-                            #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-                            #[serde(tag = "type", content = "payload")]
-                            enum BridgeDutyInterop {
-                                SignDeposit(DepositInfoInterop),
-                                FulfillWithdrawal(CooperativeWithdrawalInfoInterop),
-                            }
-
-                            let duty_interop: BridgeDutyInterop =
+                            let duty_interop: BridgeDuty =
                                 serde_json::from_str(&serde_json::to_string(&duty).unwrap())
                                     .unwrap();
 
-                            let duty = match duty_interop {
-                                BridgeDutyInterop::SignDeposit(deposit_info) => {
-                                    let deposit_info = DepositInfo::new(
-                                        deposit_info.deposit_request_outpoint,
-                                        stake_index, // FIXME: UPDATE THIS FOR NEW STAKE
-                                        deposit_info.el_address,
-                                        deposit_info.total_amount,
-                                        deposit_info.take_back_leaf_hash,
-                                        deposit_info
-                                            .original_taproot_addr
-                                            .address()
-                                            .script_pubkey(),
-                                    );
-                                    stake_index += 1;
-                                    BridgeDuty::SignDeposit(deposit_info)
-                                }
-                                BridgeDutyInterop::FulfillWithdrawal(
-                                    cooperative_withdrawal_info,
-                                ) => {
-                                    let withdrawal_info = WithdrawalInfo::new(
-                                        cooperative_withdrawal_info.deposit_outpoint,
-                                        cooperative_withdrawal_info.user_destination,
-                                        cooperative_withdrawal_info.assigned_operator_idx,
-                                        cooperative_withdrawal_info.exec_deadline,
-                                    );
-                                    BridgeDuty::FulfillWithdrawal(withdrawal_info)
-                                }
-                            };
-                            duty_sender.send(duty).expect("should be able to send duty");
+                            duty_sender
+                                .send(duty_interop)
+                                .expect("should be able to send duty");
                         }
 
                         db.set_last_fetched_duty_index(stop_index).await.unwrap(); // FIXME: Handle
