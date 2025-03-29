3ae39b388c946af1a20c224123cc8d4a24246e41
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-03-29 16:02:34 +0800

    remove connector_k from ClaimTx::finalize arguments

--- crates/tx-graph/src/peg_out_graph.rs
+++ crates/tx-graph/src/peg_out_graph.rs
@@ -696,7 +696,7 @@ mod tests {
             deposit_txid,
             withdrawal_fulfillment_txid.as_byte_array(),
         );
-        let signed_claim_tx = claim_tx.finalize(*claim_sig, kickoff);
+        let signed_claim_tx = claim_tx.finalize(*claim_sig);
         info!(
             vsize = signed_claim_tx.vsize(),
             action = "broadcasting claim tx",
@@ -1439,7 +1439,7 @@ mod tests {
             deposit_txid,
             withdrawal_fulfillment_txid.as_byte_array(),
         );
-        let signed_claim_tx = claim_tx.finalize(*claim_sig, kickoff);
+        let signed_claim_tx = claim_tx.finalize(*claim_sig);
         info!(vsize = signed_claim_tx.vsize(), "broadcasting claim tx");
 
         let claim_child_tx = create_cpfp_child(
@@ -1868,7 +1868,7 @@ mod tests {
             deposit_txid,
             withdrawal_fulfillment_txid.as_byte_array(),
         );
-        let signed_ongoing_claim_tx = ongoing_claim_tx.finalize(*claim_sig, kickoff);
+        let signed_ongoing_claim_tx = ongoing_claim_tx.finalize(*claim_sig);
         info!(
             action = "broadcasting claim tx",
             vsize = signed_ongoing_claim_tx.vsize(),
