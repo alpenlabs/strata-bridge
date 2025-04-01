338f0275d9cd26409a5157793601eaf3f3be058c
Jose Storopoli <jose@storopoli.com>
2025-04-01 15:47:24 +0800

    operator wallet: refactor + btcio rpc

--- bin/alpen-bridge/src/mode/operator.rs
+++ bin/alpen-bridge/src/mode/operator.rs
@@ -7,7 +7,7 @@ use std::{
 };
 
 use anyhow::anyhow;
-use bdk_bitcoind_rpc::bitcoincore_rpc::{self, RpcApi};
+use bdk_bitcoind_rpc::bitcoincore_rpc;
 use bitcoin::{
     consensus,
     hashes::Hash,
@@ -37,8 +37,12 @@ use strata_bridge_db::{persistent::sqlite::SqliteDb, public::PublicDb};
 use strata_bridge_p2p_service::{
     bootstrap as p2p_bootstrap, Configuration as P2PConfiguration, MessageHandler,
 };
-use strata_bridge_primitives::constants::SEGWIT_MIN_AMOUNT;
+use strata_bridge_primitives::{constants::SEGWIT_MIN_AMOUNT, types::OperatorIdx};
 use strata_bridge_stake_chain::prelude::OPERATOR_FUNDS;
+use strata_btcio::rpc::{
+    traits::{BroadcasterRpc, ReaderRpc},
+    BitcoinClient,
+};
 use strata_p2p::swarm::handle::P2PHandle;
 use strata_p2p_types::P2POperatorPubKey;
 use tokio::{spawn, task::JoinHandle, try_join};
@@ -63,6 +67,7 @@ pub(crate) async fn bootstrap(params: Params, config: Config) -> anyhow::Result<
         .secret_key()
         .await
         .map_err(|e| anyhow!("error while asking for p2p key: {e:?}"))?;
+    let my_key = s2_client.musig2_signer().pubkey().await?;
 
     info!(
         "Retrieved P2P secret key from S2: {sk_fingerprint:?}",
@@ -72,148 +77,37 @@ pub(crate) async fn bootstrap(params: Params, config: Config) -> anyhow::Result<
     // Database instances.
     let db = init_database_handle(&config).await;
     let db_rpc = db.clone();
+    let db_stakechain = db.clone();
 
-    // BitcoinD RPC client for the Operator Wallet.
-    let auth = bitcoincore_rpc::Auth::UserPass(
+    // Create the async BitcoinD RPC client.
+    let bitcoin_rpc_client = Arc::new(BitcoinClient::new(
+        config.btc_client.url.to_string(),
         config.btc_client.user.to_string(),
         config.btc_client.pass.to_string(),
-    );
-    let bitcoin_rpc_client = Arc::new(
-        bitcoincore_rpc::Client::new(config.btc_client.url.as_str(), auth)
-            .expect("should be able to create bitcoin client"),
-    );
-    info!(?bitcoin_rpc_client, "bitcoin rpc client");
+        config.btc_client.retry_count,
+        config.btc_client.retry_interval,
+    )?);
 
-    // Operator wallet stuff.
-    let general_key = s2_client.general_wallet_signer().pubkey().await?;
-    info!(%general_key, "operator wallet general key");
-    let stakechain_key = s2_client.stakechain_wallet_signer().pubkey().await?;
-    info!(%stakechain_key, "operator wallet stakechain key");
-    let my_key = s2_client.musig2_signer().pubkey().await?;
-    info!(%my_key, "MuSig2 operator key");
+    // Initialize the operator wallet.
+    let mut operator_wallet = init_operator_wallet(&config, &params, s2_client.clone()).await?;
+
+    // Get the operator's key index.
     let my_index = params
         .keys
         .musig2
         .iter()
         .position(|k| k == &my_key)
-        .expect("should be able to find my index");
-    info!(%my_index, "my index");
-    let operator_wallet_config = OperatorWalletConfig::new(
-        OPERATOR_FUNDS,
-        // NOTE: 32 seems an OK-ish pool size for the operator wallet.
-        //       These will be refilled that's why its a 'pool'.
-        32,
-        SEGWIT_MIN_AMOUNT,
-        params.stake_chain.stake_amount,
-        params.network,
-    );
+        .expect("should be able to find my index") as u32;
 
-    let sync_backend = Backend::BitcoinCore(bitcoin_rpc_client.clone());
-    info!(?sync_backend, "operator wallet sync backend");
-    let mut operator_wallet = OperatorWallet::new(
-        general_key,
-        stakechain_key,
-        operator_wallet_config,
-        sync_backend,
-    );
-    info!(?operator_wallet, "created operator wallet");
-
-    // Handle the pre-stake tx.
-    if db
-        .get_pre_stake(my_index as u32)
-        .await
-        .expect("should be able to consult the database")
-        .is_none()
-    {
-        // This means that we don't have a pre-stake tx in the database.
-        // We need to create a pre-stake tx, sign it, broadcast it and save it to the database.
-        info!("no pre-stake tx in the database, creating one");
-        // BitcoinD is ancient technology, so we need to convert the fee rate estimate to a proper
-        // FeeRate.
-        let fee_rate = bitcoin_rpc_client
-            .estimate_smart_fee(1, None)
-            .expect("should be able to get the fee rate estimate")
-            .fee_rate
-            .and_then(|per_kw| per_kw.checked_div(1_000))
-            .and_then(|per_vb| FeeRate::from_sat_per_vb(per_vb.to_sat()))
-            .unwrap_or(FeeRate::from_sat_per_vb_unchecked(3));
-
-        info!(?fee_rate, "fee rate");
-
-        // We need to sync the wallet.
-        info!("syncing the operator wallet");
-        operator_wallet
-            .sync()
-            .await
-            .expect("should be able to sync the wallet");
-        info!("synced the operator wallet");
-
-        // Create the PreStake tx.
-        let pre_stake_psbt = operator_wallet
-            .create_prestake_tx(fee_rate)
-            .expect("should be able to create the pre-stake tx");
-        // Get the unsigned pre-stake tx.
-        let pre_stake_tx = pre_stake_psbt.unsigned_tx;
-        let pre_stake_txid = pre_stake_tx.compute_txid();
-        info!(%pre_stake_txid, "created the pre-stake tx");
-
-        // Collect all the UTXOs in the stakechain wallet that match the pre-stake tx inputs.
-        const VOUT: u32 = 0;
-        let general_wallet = operator_wallet.general_wallet();
-        let prevouts = pre_stake_tx
-            .input
-            .iter()
-            .map(|i| {
-                let outpoint = i.previous_output;
-                info!(?outpoint, "outpoint");
-                general_wallet
-                    .get_utxo(outpoint)
-                    .expect("should be able to get the outpoint")
-                    .txout
-            })
-            .collect::<Vec<TxOut>>();
-        info!(?prevouts, "prevouts");
-        let prevouts = Prevouts::All(&prevouts);
-        let mut sighasher = SighashCache::new(pre_stake_tx);
-        let sighash = sighasher
-            .taproot_key_spend_signature_hash(VOUT as usize, &prevouts, TapSighashType::Default)
-            .expect("must be able to compute the sighash");
-
-        // Sign the pre-stake tx.
-        let signature = s2_client
-            .general_wallet_signer()
-            .sign(&sighash.to_byte_array(), None)
-            .await?;
-
-        // let signature = taproot::Signature {
-        //     signature,
-        //     sighash_type: TapSighashType::Default,
-        // };
-        sighasher
-            .witness_mut(0)
-            .expect("must be able to get the witness")
-            .push(signature.serialize());
-        let signed_pre_stake_tx = sighasher.into_transaction();
-        info!(%pre_stake_txid, "signed the pre-stake tx");
-        info!(signed_pre_stake_tx = %consensus::encode::serialize_hex(&signed_pre_stake_tx), "signed pre-stake tx");
-        dbg!(&signed_pre_stake_tx);
-
-        // Broadcast the pre-stake tx.
-        bitcoin_rpc_client
-            .send_raw_transaction(&signed_pre_stake_tx)
-            .expect("should be able to broadcast the pre-stake tx");
-        info!(%pre_stake_txid, "broadcasted the pre-stake tx");
-
-        // Save the pre-stake tx to the database.
-        let pre_stake_outpoint = OutPoint {
-            txid: pre_stake_txid,
-            vout: VOUT,
-        };
-        db.set_pre_stake(my_index as u32, pre_stake_outpoint)
-            .await
-            .expect("should be able to save the pre-stake tx to the database");
-        info!(%pre_stake_txid, "saved the pre-stake tx to the database");
-    }
+    // Handle the stakechain genesis.
+    handle_stakechain_genesis(
+        db_stakechain,
+        s2_client.clone(),
+        &mut operator_wallet,
+        my_index,
+        bitcoin_rpc_client.clone(), // duty tracker also needs an Arc<BitcoinClient>
+    )
+    .await;
 
     // P2P message handler.
     let (message_handler, p2p_task) = init_p2p_msg_handler(&config, &params, sk).await?;
@@ -431,4 +325,164 @@ async fn start_rpc_server(
             .expect("failed to start RPC server");
     });
     Ok(handle)
+}
+
+async fn init_operator_wallet(
+    config: &Config,
+    params: &Params,
+    s2_client: SecretServiceClient,
+) -> anyhow::Result<OperatorWallet> {
+    // BitcoinD RPC client for the Operator Wallet.
+    let auth = bitcoincore_rpc::Auth::UserPass(
+        config.btc_client.user.to_string(),
+        config.btc_client.pass.to_string(),
+    );
+    let bitcoin_rpc_client = Arc::new(
+        bitcoincore_rpc::Client::new(config.btc_client.url.as_str(), auth)
+            .expect("should be able to create bitcoin client"),
+    );
+    info!(?bitcoin_rpc_client, "bitcoin rpc client");
+
+    // Operator wallet stuff.
+    let general_key = s2_client.general_wallet_signer().pubkey().await?;
+    info!(%general_key, "operator wallet general key");
+    let stakechain_key = s2_client.stakechain_wallet_signer().pubkey().await?;
+    info!(%stakechain_key, "operator wallet stakechain key");
+    let my_key = s2_client.musig2_signer().pubkey().await?;
+    info!(%my_key, "MuSig2 operator key");
+    let my_index = params
+        .keys
+        .musig2
+        .iter()
+        .position(|k| k == &my_key)
+        .expect("should be able to find my index");
+    info!(%my_index, "my index");
+    let operator_wallet_config = OperatorWalletConfig::new(
+        OPERATOR_FUNDS,
+        // NOTE: 32 seems an OK-ish pool size for the operator wallet.
+        //       These will be refilled that's why its a 'pool'.
+        32,
+        SEGWIT_MIN_AMOUNT,
+        params.stake_chain.stake_amount,
+        params.network,
+    );
+
+    let sync_backend = Backend::BitcoinCore(bitcoin_rpc_client.clone());
+    info!(?sync_backend, "operator wallet sync backend");
+    let operator_wallet = OperatorWallet::new(
+        general_key,
+        stakechain_key,
+        operator_wallet_config,
+        sync_backend,
+    );
+    info!(?operator_wallet, "created operator wallet");
+
+    Ok(operator_wallet)
+}
+
+/// Handles the stakechain genesis.
+///
+/// If the pre-stake tx is not in the database, this function will create a pre-stake tx, sign it,
+/// broadcast it and save it to the database.
+async fn handle_stakechain_genesis(
+    db: SqliteDb,
+    s2_client: SecretServiceClient,
+    operator_wallet: &mut OperatorWallet,
+    my_index: OperatorIdx,
+    bitcoin_rpc_client: Arc<BitcoinClient>,
+) {
+    if db
+        .get_pre_stake(my_index)
+        .await
+        .expect("should be able to consult the database")
+        .is_none()
+    {
+        // This means that we don't have a pre-stake tx in the database.
+        // We need to create a pre-stake tx, sign it, broadcast it and save it to the database.
+        info!("no pre-stake tx in the database, creating one");
+        let fee_rate = bitcoin_rpc_client
+            .estimate_smart_fee(1)
+            .await
+            .expect("should be able to get the fee rate estimate");
+
+        let fee_rate = FeeRate::from_sat_per_vb_unchecked(fee_rate);
+
+        info!(?fee_rate, "fee rate");
+
+        // We need to sync the wallet.
+        info!("syncing the operator wallet");
+        operator_wallet
+            .sync()
+            .await
+            .expect("should be able to sync the wallet");
+        info!("synced the operator wallet");
+
+        // Create the PreStake tx.
+        let pre_stake_psbt = operator_wallet
+            .create_prestake_tx(fee_rate)
+            .expect("should be able to create the pre-stake tx");
+        // Get the unsigned pre-stake tx.
+        let pre_stake_tx = pre_stake_psbt.unsigned_tx;
+        let pre_stake_txid = pre_stake_tx.compute_txid();
+        info!(%pre_stake_txid, "created the pre-stake tx");
+
+        // Collect all the UTXOs in the stakechain wallet that match the pre-stake tx inputs.
+        const VOUT: u32 = 0;
+        let general_wallet = operator_wallet.general_wallet();
+        let prevouts = pre_stake_tx
+            .input
+            .iter()
+            .map(|i| {
+                let outpoint = i.previous_output;
+                info!(?outpoint, "outpoint");
+                general_wallet
+                    .get_utxo(outpoint)
+                    .expect("should be able to get the outpoint")
+                    .txout
+            })
+            .collect::<Vec<TxOut>>();
+        info!(?prevouts, "prevouts");
+        let prevouts = Prevouts::All(&prevouts);
+        let mut sighasher = SighashCache::new(pre_stake_tx);
+        let sighash = sighasher
+            .taproot_key_spend_signature_hash(VOUT as usize, &prevouts, TapSighashType::Default)
+            .expect("must be able to compute the sighash");
+
+        // Sign the pre-stake tx.
+        let signature = s2_client
+            .general_wallet_signer()
+            .sign(&sighash.to_byte_array(), None)
+            .await
+            .expect("should be able to sign the pre-stake tx");
+
+        // let signature = taproot::Signature {
+        //     signature,
+        //     sighash_type: TapSighashType::Default,
+        // };
+        sighasher
+            .witness_mut(0)
+            .expect("must be able to get the witness")
+            .push(signature.serialize());
+        let signed_pre_stake_tx = sighasher.into_transaction();
+        info!(%pre_stake_txid, "signed the pre-stake tx");
+        info!(signed_pre_stake_tx = %consensus::encode::serialize_hex(&signed_pre_stake_tx), "signed pre-stake tx");
+        dbg!(&signed_pre_stake_tx);
+
+        // Broadcast the pre-stake tx.
+        bitcoin_rpc_client
+            .send_raw_transaction(&signed_pre_stake_tx)
+            .await
+            .expect("should be able to broadcast the pre-stake tx");
+        info!(%pre_stake_txid, "broadcasted the pre-stake tx");
+
+        // Save the pre-stake tx to the database.
+        let pre_stake_outpoint = OutPoint {
+            txid: pre_stake_txid,
+            vout: VOUT,
+        };
+        db.set_pre_stake(my_index, pre_stake_outpoint)
+            .await
+            .expect("should be able to save the pre-stake tx to the database");
+        info!(%pre_stake_txid, "saved the pre-stake tx to the database");
+    }
 }
