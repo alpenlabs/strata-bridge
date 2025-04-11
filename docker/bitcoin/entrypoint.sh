aff5f71c85e897e6e0a9b48a2caea2ea74e763d1
Rajil Bajracharya <imrb1213@gmail.com>
2025-04-11 23:59:09 +0545

    refactor(docker): use conf instead of args for bitcoind

--- docker/bitcoin/entrypoint.sh
+++ docker/bitcoin/entrypoint.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-bitcoind -daemon -server -txindex -printtoconsole -regtest -rpcallowip=0.0.0.0/0 -rpcbind=172.28.1.8:18443 -rpcuser=user -rpcpassword=password -zmqpubhashblock=tcp://172.28.1.8:28332 -zmqpubhashtx=tcp://172.28.1.8:28333 -zmqpubrawblock=tcp://172.28.1.8:28334 -zmqpubrawtx=tcp://172.28.1.8:28335 -zmqpubsequence=tcp://172.28.1.8:28336 -fallbackfee=0.00001 -debug=zmq -debuglogfile=/home/bitcoin/daemon.log
+BITCOIND_CONF_FILE=/home/bitcoin/bitcoin.conf
+
+# Generate bitcoin.conf
+cat <<EOF > ${BITCOIND_CONF_FILE}
+regtest=1
+
+[regtest]
+rpcuser=user
+rpcpassword=password
+rpcbind=172.28.1.8:18443
+rpcallowip=0.0.0.0/0
+fallbackfee=0.00001
+server=1
+txindex=1
+printtoconsole=1
+acceptnonstdtxn=1
+minrelaytxfee=0.0
+blockmintxfee=0.0
+dustRelayFee=0.0
+debug=zmq
+debuglogfile=/home/bitcoin/daemon.log
+zmqpubhashblock=tcp://172.28.1.8:28332
+zmqpubhashtx=tcp://172.28.1.8:28333
+zmqpubrawblock=tcp://172.28.1.8:28334
+zmqpubrawtx=tcp://172.28.1.8:28335
+zmqpubsequence=tcp://172.28.1.8:28336
+EOF
+
+bitcoind -daemon -conf=${BITCOIND_CONF_FILE}
 
 sleep 1
 
