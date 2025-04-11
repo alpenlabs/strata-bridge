2f9bbfc24b2ede611acd0838776b569e68a9fd8c
Rajil Bajracharya <imrb1213@gmail.com>
2025-04-11 18:43:35 +0545

    feat(docker): send funds to stake chain wallets upon startup

--- docker/bitcoin/entrypoint.sh
+++ docker/bitcoin/entrypoint.sh
@@ -17,9 +17,23 @@ sleep 0.1
 
 # mine enough blocks to the default wallet address to mature coinbase funds
 bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 createwallet default
-bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 loadwallet default
 MY_ADDRESS=$(bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 -rpcwallet=default getnewaddress)
-bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 generatetoaddress 101 $MY_ADDRESS
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 generatetoaddress 104 $MY_ADDRESS
+
+# send some funds to the stake chain wallet too
+FUNDING_AMOUNT="0.00027720"
+echo "sending ${FUNDING_AMOUNT} BTC to the stake chain wallet"
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 sendtoaddress bcrt1pn049qtjwy9ppz7ljjl9r3tmp60et9y0t3cdkdame4g0kldjj5dqs7t87hf ${FUNDING_AMOUNT}
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 generatetoaddress 1 $MY_ADDRESS
+sleep 0.1
+
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 sendtoaddress bcrt1pga206rnnd9vchqvs5s0gax4v99pdzlml470f3tfw8nfaemmmu0wsjru24g ${FUNDING_AMOUNT}
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 generatetoaddress 1 $MY_ADDRESS
+sleep 0.1
+
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 sendtoaddress bcrt1pmpv05lwwpdncjjtglmrccj4q75krhrskhvkur4af7t3dma0s6hhsl7j55p ${FUNDING_AMOUNT}
+bitcoin-cli -rpcuser=user -rpcpassword=password -regtest -rpcconnect=172.28.1.8 -rpcport=18443 generatetoaddress 1 $MY_ADDRESS
+sleep 0.1
 
 # Run forever
 tail -f /dev/null
