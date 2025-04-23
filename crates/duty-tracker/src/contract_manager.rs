e96763773dedc1911c8392570aac38c0f92f9ac2
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-04-23 15:25:05 -0600

    remove unused ResourceManagers context

--- crates/duty-tracker/src/contract_manager.rs
+++ crates/duty-tracker/src/contract_manager.rs
@@ -226,7 +226,6 @@ impl ContractManager {
                 cfg: cfg.clone(),
                 state,
                 state_handles,
-                resource_mgrs: todo!(),
             };
 
             while cursor < current {
@@ -263,7 +262,7 @@ impl ContractManager {
 
             let mut block_sub = zmq_client.subscribe_blocks().await;
             let mut interval = time::interval(nag_interval);
-            let pov_key = ctx.cfg.operator_table.pov_op_key();
+            let pov_key = ctx.cfg.operator_table.pov_op_key().clone();
             let pov_idx = ctx.cfg.operator_table.pov_idx();
             loop {
                 let mut duties = vec![];
@@ -353,10 +352,6 @@ impl Drop for ContractManager {
     }
 }
 
-struct ResourceManagers {
-    s2_client: SecretServiceClient,
-}
-
 /// The handles required by the duty tracker to execute duties.
 struct OutputHandles {
     wallet: RwLock<OperatorWallet>,
@@ -396,7 +391,6 @@ struct ContractManagerCtx {
     cfg: ExecutionConfig,
     state_handles: StateHandles,
     state: ExecutionState,
-    resource_mgrs: ResourceManagers,
 }
 
 impl ContractManagerCtx {
