5f948d9906fd2f6d3dcebc419554336683881de4
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-04-23 13:13:02 -0600

    fix missing documentation warnings

--- crates/duty-tracker/src/s2_session_manager.rs
+++ crates/duty-tracker/src/s2_session_manager.rs
@@ -1,20 +1,28 @@
+//! This module implements a session state management system for musig sessions around a given
+//! underlying SecretServiceClient.
 use bitcoin::{OutPoint, XOnlyPublicKey};
 use musig2::{secp256k1::Message, LiftedSignature, PartialSignature, PubNonce};
 use secret_service_client::SecretServiceClient;
 use thiserror::Error;
 
+/// System for managing session state for musig sessions.
+#[derive(Debug, Clone)]
 pub struct MusigSessionManager {
+    /// The underlying S2 client.
     pub s2_client: SecretServiceClient,
 }
 impl MusigSessionManager {
+    /// Makes a new MusigSessionManager from a SecretServiceClient.
     pub fn new(s2_client: SecretServiceClient) -> Self {
         MusigSessionManager { s2_client }
     }
 
+    /// Given an OutPoint, get back a PubNonce for that session
     pub async fn get_nonce(&self, outpoint: OutPoint) -> Result<PubNonce, MusigSessionErr> {
         todo!()
     }
 
+    /// Load a PubNonce into the signing session identified by OutPoint.
     pub async fn put_nonce(
         &self,
         outpoint: OutPoint,
@@ -24,6 +32,7 @@ impl MusigSessionManager {
         todo!()
     }
 
+    /// Given an OutPoint and the sighash for what is being signed get our PartialSignature.
     pub async fn get_partial(
         &self,
         outpoint: OutPoint,
@@ -32,6 +41,7 @@ impl MusigSessionManager {
         todo!()
     }
 
+    /// Load a PartialSignature into the signing session identified by OutPoint.
     pub async fn put_partial(
         &self,
         outpoint: OutPoint,
@@ -41,6 +51,7 @@ impl MusigSessionManager {
         todo!()
     }
 
+    /// Finalize the musig signing process and extract the final LiftedSignature
     pub async fn get_signature(
         &self,
         outpoint: OutPoint,
@@ -48,11 +59,13 @@ impl MusigSessionManager {
         todo!()
     }
 
+    /// Delete all session state associated with the given OutPoint.
     pub fn drop_session(&self, outpoint: OutPoint) {
         todo!()
     }
 }
 
+/// Error type that encapsulates all of the things that can go wrong with the Musig signing process.
 #[derive(Debug, Error)]
 pub enum MusigSessionErr {
     /// Errors from failed secret service requests
