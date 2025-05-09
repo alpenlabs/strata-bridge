61ac14eb298efcfcce558d709652dff980340af1
Rajil Bajracharya <imrb1213@gmail.com>
2025-05-10 03:21:26 +0545

    feat(duty-tracker): make multi-threaded graph gen opt-in

--- crates/duty-tracker/src/contract_state_machine.rs
+++ crates/duty-tracker/src/contract_state_machine.rs
@@ -1171,45 +1171,54 @@ impl ContractSM {
                     return Ok(vec![]);
                 }
 
-                let shared_cfg = Arc::new(self.cfg.clone());
-                let jobs = peg_out_graph_inputs
-                    .iter()
-                    .map(|(signer, input)| {
-                        let thread_cfg = shared_cfg.clone();
-                        let input = input.clone();
-                        (
-                            signer,
-                            // TODO(proofofkeags): use async thread pool in future commit.
-                            //
-                            // This is currently implemented as an OS thread for a couple of
-                            // reasons. First, we'd like to be able to test this without having to
-                            // invoke an async runtime. As of right now this is inside of a pure
-                            // function which means its testing requirements are a little bit more
-                            // relaxed. Secondly, the value of async is much less pronounced for
-                            // operations that are waiting on compute instead of IO.
-                            thread::Builder::new()
-                                .stack_size(8 * 1024 * 1024)
-                                .spawn(move || {
-                                    debug!(
-                                        stake_txid = %input.stake_outpoint.txid,
-                                        "building graph..."
-                                    );
-                                    thread_cfg.build_graph(input)
-                                })
-                                .expect("spawn succeeds"),
-                        )
-                    })
-                    .collect::<BTreeMap<_, _>>();
+                let graphs = if std::env::var("MULTI_THREAD_GRAPH_GEN").is_ok_and(|v| v == "1") {
+                    let shared_cfg = Arc::new(self.cfg.clone());
 
-                let graphs = jobs
-                    .into_iter()
-                    .map(|(signer, job)| {
-                        (
-                            signer,
-                            job.join().expect("peg out graph generation panic'ed"),
-                        )
-                    })
-                    .collect::<BTreeMap<_, _>>();
+                    let jobs = peg_out_graph_inputs
+                        .iter()
+                        .map(|(signer, input)| {
+                            let thread_cfg = shared_cfg.clone();
+                            let input = input.clone();
+                            (
+                                signer,
+                                // TODO(proofofkeags): use async thread pool in future commit.
+                                //
+                                // This is currently implemented as an OS thread for a couple of
+                                // reasons. First, we'd like to be able to test this without having
+                                // to invoke an async runtime. As
+                                // of right now this is inside of a pure
+                                // function which means its testing requirements are a little bit
+                                // more relaxed. Secondly, the
+                                // value of async is much less pronounced for
+                                // operations that are waiting on compute instead of IO.
+                                thread::Builder::new()
+                                    .stack_size(8 * 1024 * 1024)
+                                    .spawn(move || {
+                                        debug!(
+                                            stake_txid = %input.stake_outpoint.txid,
+                                            "building graph..."
+                                        );
+                                        thread_cfg.build_graph(input)
+                                    })
+                                    .expect("spawn succeeds"),
+                            )
+                        })
+                        .collect::<BTreeMap<_, _>>();
+
+                    jobs.into_iter()
+                        .map(|(signer, job)| {
+                            (
+                                signer,
+                                job.join().expect("peg out graph generation panic'ed"),
+                            )
+                        })
+                        .collect::<BTreeMap<_, _>>()
+                } else {
+                    peg_out_graph_inputs
+                        .iter()
+                        .map(|(signer, input)| (signer, self.cfg.build_graph(input.clone())))
+                        .collect::<BTreeMap<_, _>>()
+                };
 
                 let duties = graphs
                     .values()
