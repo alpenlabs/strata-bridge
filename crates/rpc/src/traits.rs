0a5127570fb54bf2d265441cdeaed6adab20756b
Rajil Bajracharya <imrb1213@gmail.com>
2025-05-16 19:44:29 +0545

    fix(rpc): use the right txid name in rpc call

--- crates/rpc/src/traits.rs
+++ crates/rpc/src/traits.rs
@@ -76,5 +76,5 @@ pub trait StrataBridgeDaApi {
 
     /// Query for disprove signature for a particular claim.
     #[method(name = "disproveSignature")]
-    async fn get_disprove_signature(&self, deposit_txid: Txid) -> RpcResult<Option<Signature>>;
+    async fn get_disprove_signature(&self, claim_txid: Txid) -> RpcResult<Option<Signature>>;
 }
