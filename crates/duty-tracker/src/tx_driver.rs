66e8200dc71702b6ef843936d46ebedd855ae5cc
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-05-20 20:14:06 -0600

    tx drive idempotence: test written

--- crates/duty-tracker/src/tx_driver.rs
+++ crates/duty-tracker/src/tx_driver.rs
@@ -144,3 +144,147 @@ impl Drop for TxDriver {
         self.driver.abort();
     }
 }
+
+#[cfg(test)]
+mod e2e_tests {
+    use std::{
+        collections::{BTreeMap, VecDeque},
+        sync::Arc,
+    };
+
+    use bitcoind_async_client::Client as BitcoinClient;
+    use btc_notify::client::{BtcZmqClient, BtcZmqConfig};
+    use corepc_node::CookieValues;
+    use futures::join;
+    use serial_test::serial;
+    use strata_bridge_common::logging::{self, LoggerConfig};
+    use tracing::{debug, info};
+
+    use super::TxDriver;
+
+    async fn setup() -> Result<(TxDriver, corepc_node::Node), Box<dyn std::error::Error>> {
+        let mut bitcoin_conf = corepc_node::Conf::default();
+        bitcoin_conf.enable_zmq = true;
+        // TODO(proofofkeags): do dynamic port allocation so these can be run in parallel
+        bitcoin_conf.args.extend(vec![
+            "-zmqpubhashblock=tcp://127.0.0.1:23882",
+            "-zmqpubhashtx=tcp://127.0.0.1:23883",
+            "-zmqpubrawblock=tcp://127.0.0.1:23884",
+            "-zmqpubrawtx=tcp://127.0.0.1:23885",
+            "-zmqpubsequence=tcp://127.0.0.1:23886",
+        ]);
+        let bitcoind = corepc_node::Node::from_downloaded_with_conf(&bitcoin_conf)?;
+        info!("corepc_node::Node initialized");
+
+        let cfg = BtcZmqConfig::default()
+            .with_hashblock_connection_string("tcp://127.0.0.1:23882")
+            .with_hashtx_connection_string("tcp://127.0.0.1:23883")
+            .with_rawblock_connection_string("tcp://127.0.0.1:23884")
+            .with_rawtx_connection_string("tcp://127.0.0.1:23885")
+            .with_sequence_connection_string("tcp://127.0.0.1:23886");
+
+        let zmq_client = BtcZmqClient::connect(&cfg, VecDeque::new()).await?;
+        info!("BtcZmqClient initialized");
+
+        let CookieValues { user, password } = bitcoind
+            .params
+            .get_cookie_values()
+            .expect("can read cookie")
+            .expect("can parse cookie");
+        let rpc_client = BitcoinClient::new(bitcoind.rpc_url(), user, password, None, None)
+            .expect("can set up rpc client");
+        info!("bitcoin_async_client::Client initialized");
+
+        let tx_driver = TxDriver::new(zmq_client, rpc_client).await;
+        info!("TxDriver initialized");
+
+        Ok((tx_driver, bitcoind))
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn tx_drive_idempotence() -> Result<(), Box<dyn std::error::Error>> {
+        logging::init(LoggerConfig::new("tx_drive_idempotence".to_string()));
+
+        let (driver, bitcoind) = setup().await?;
+
+        let new_address = bitcoind.client.new_address()?;
+        // Mine 101 new blocks to that same address. We use 101 so that the coins minted in the
+        // first block can be spent which we will need to do for the remainder of the test.
+        let _ = bitcoind
+            .client
+            .generate_to_address(101, &new_address)?
+            .into_model()?;
+        debug!("waiting for test funds to mature");
+        wait_for_height(&bitcoind, 101).await?;
+        debug!("test funds matured");
+
+        debug!("creating raw transaction");
+        let mut outs = BTreeMap::new();
+        outs.insert(new_address.to_string(), 1.0);
+        let raw = bitcoind.client.create_raw_transaction(&[], &outs)?.0;
+        debug!(?raw, "created raw transaction");
+
+        debug!("funding raw transaction");
+        let funded = bitcoind.client.fund_raw_transaction(&raw)?.hex;
+        debug!(%funded, "funded raw transaction");
+
+        debug!("signing raw transaction");
+        let signed = bitcoind
+            .client
+            .sign_raw_transaction_with_wallet(&funded)?
+            .into_model()?
+            .raw_transaction;
+        debug!(?signed, "signed raw transaction");
+
+        info!("sending first copy to TxDriver");
+        let fst = driver.drive(signed.clone());
+        info!("sending second copy to TxDriver");
+        let snd = driver.drive(signed);
+
+        info!("starting mining task");
+        let stop = Arc::new(std::sync::atomic::AtomicBool::new(true));
+        let stop_thread = stop.clone();
+        let mine_task = tokio::task::spawn_blocking(move || {
+            while stop_thread.load(std::sync::atomic::Ordering::SeqCst) {
+                bitcoind
+                    .client
+                    .generate_to_address(1, &new_address)
+                    .unwrap();
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+        });
+
+        debug!("waiting for TxDriver::drive calls to complete");
+        let (fst_res, snd_res) = join!(fst, snd);
+        info!("TxDriver::drive calls completed");
+
+        debug!("terminating mining task");
+        stop.store(false, std::sync::atomic::Ordering::SeqCst);
+        tokio::time::timeout(std::time::Duration::from_secs(1), mine_task).await??;
+        info!("mining task terminated");
+
+        fst_res.expect("first drive succeeds");
+        snd_res.expect("second drive succeeds");
+
+        Ok(())
+    }
+
+    // This is disabled because it is merely a testing helper function to ensure tests complete in
+    // a timely manner, so we don't want lack of full coverage in this function to distract from
+    // overall coverage.
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    async fn wait_for_height(
+        rpc_client: &corepc_node::Node,
+        height: usize,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(
+            tokio::time::timeout(std::time::Duration::from_secs(10), async {
+                while rpc_client.client.get_blockchain_info().unwrap().blocks != height as i64 {
+                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                }
+            })
+            .await?,
+        )
+    }
+}
