85eb2a06a69a1f87e561c71a51fcdf766646ba5d
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-05-22 14:03:37 -0600

    document and flesh out Semigroup/Monoid

--- crates/algebra/src/monoid.rs
+++ crates/algebra/src/monoid.rs
@@ -1,13 +1,27 @@
 use crate::semigroup::Semigroup;
 
+/// A [`Monoid`] is an algebraic structure with [`Semigroup`] properties as well as equipped with
+/// a single identity element. The key property of the [`Monoid::empty`] value is as follows:
+///
+/// a: T
+/// T: Monoid
+/// T::empty().merge(a) == a.merge(T::empty()) == a
+///
+/// Intuitively this means that giving [`Monoid::empty`] as an argument to [`Semigroup::merge`] MUST
+/// return the merge's other argument.
 pub trait Monoid: Semigroup {
     fn empty() -> Self;
 }
 
+/// [`concat`] is the free catamorphism (fold) over a Monoidal iterator. If the iterator has no
+/// elements, it returns [`Monoid::empty`], otherwise it folds the iterator using
+/// [`Semigroup::merge`].
 pub fn concat<T: Monoid>(xs: impl IntoIterator<Item = T>) -> T {
     xs.into_iter().fold(T::empty(), <T as Semigroup>::merge)
 }
 
+/// [`fold_map`] is the universal catamorphism over all iterators. If given the iterant type has a
+/// morphism into a Monoidal structure this function will use that Monoidal structure to fold.
 pub fn fold_map<T: Monoid, U, Iter: IntoIterator<Item = U>, F: FnMut(U) -> T>(f: F, xs: Iter) -> T {
     concat(xs.into_iter().map(f))
 }
--- crates/algebra/src/semigroup.rs
+++ crates/algebra/src/semigroup.rs
@@ -1,3 +1,26 @@
+/// A [`Semigroup`] is an algebraic structure that is closed under a binary associative operation.
+/// To define a lawful semigroup impl you must define the [`Semigroup::merge`] operation. The
+/// requirement is as follows:
+///
+/// a: T
+/// b: T
+/// c: T
+/// a.merge(b).merge(c) == a.merge(b.merge(c))
 pub trait Semigroup {
+    /// The canonical semigroup operation. This operation is associative and linear in both
+    /// arguments.
     fn merge(self, other: Self) -> Self;
+}
+
+/// A folding operation over an iterator that uses the `T`'s [`Semigroup::merge`] function as the
+/// folding function.
+pub fn sconcat<T: Semigroup>(xs: impl IntoIterator<Item = T>) -> Option<T> {
+    let mut res: Option<T> = None;
+    for x in xs {
+        res = Some(match res {
+            Some(acc) => acc.merge(x),
+            None => x,
+        })
+    }
+    res
 }
