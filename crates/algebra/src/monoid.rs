766a36f1311f08b7f83eee0e8f0e022090b82ba6
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-05-22 14:44:12 -0600

    fix doc compilation

--- crates/algebra/src/monoid.rs
+++ crates/algebra/src/monoid.rs
@@ -13,15 +13,14 @@ pub trait Monoid: Semigroup {
     fn empty() -> Self;
 }
 
-/// [`concat`] is the free catamorphism (fold) over a Monoidal iterator. If the iterator has no
-/// elements, it returns [`Monoid::empty`], otherwise it folds the iterator using
-/// [`Semigroup::merge`].
+/// The free catamorphism (fold) over a Monoidal iterator. If the iterator has no elements, it
+/// returns [`Monoid::empty`], otherwise it folds the iterator using [`Semigroup::merge`].
 pub fn concat<T: Monoid>(xs: impl IntoIterator<Item = T>) -> T {
     xs.into_iter().fold(T::empty(), <T as Semigroup>::merge)
 }
 
-/// [`fold_map`] is the universal catamorphism over all iterators. If given the iterant type has a
-/// morphism into a Monoidal structure this function will use that Monoidal structure to fold.
+/// The universal catamorphism over all iterators. If given the iterant type has a morphism into a
+/// Monoidal structure this function will use that Monoidal structure to fold.
 pub fn fold_map<T: Monoid, U, Iter: IntoIterator<Item = U>, F: FnMut(U) -> T>(f: F, xs: Iter) -> T {
     concat(xs.into_iter().map(f))
 }
