4129e4089cec4da59b37ac397385e39f26a83ad8
Jose Storopoli <jose@storopoli.com>
2025-05-23 11:08:40 -0300

    fix!(db-sqlite): operatordb cleanup

--- migrations/20241113060253_init_schema.sql
+++ migrations/20241113060253_init_schema.sql
@@ -34,6 +34,15 @@ CREATE TABLE IF NOT EXISTS signatures (
     PRIMARY KEY (operator_id, txid, input_index)  -- Compound primary key
 );
 
+-- Table for nonces with a compound index on (operator_id, txid, input_index)
+CREATE TABLE IF NOT EXISTS nonces (
+    operator_id INTEGER NOT NULL,
+    txid TEXT NOT NULL,          -- Store as hex string
+    input_index INTEGER NOT NULL,
+    nonce TEXT NOT NULL    ,     -- Store as hex string
+    PRIMARY KEY (operator_id, txid, input_index)  -- Compound primary key
+);
+
 -- Table for deposits with a primary key on deposit_txid mapping to an index that increments monotonically.
 CREATE TABLE IF NOT EXISTS deposits (
     deposit_txid TEXT PRIMARY KEY,  -- Store as hex string
@@ -102,71 +111,39 @@ CREATE TABLE IF NOT EXISTS pre_assert_txid_to_operator_and_deposit (
     deposit_txid TEXT NOT NULL             -- Store as hex string
 
 );
--- Table to store collected public nonces for each operator
-CREATE TABLE collected_pubnonces (
+-- Table to store public nonces for each operator
+CREATE TABLE IF NOT EXISTS pub_nonces (
+    operator_id INTEGER NOT NULL,
     txid TEXT NOT NULL,
     input_index INTEGER NOT NULL,
-    operator_id INTEGER NOT NULL,
     pubnonce TEXT NOT NULL,
-    PRIMARY KEY (txid, input_index, operator_id)
+    PRIMARY KEY (operator_id, txid, input_index)
 );
 
--- Table to store secp256k1::SecNonce data
-CREATE TABLE sec_nonces (
+-- Table to store aggregated nonces for each operator
+CREATE TABLE IF NOT EXISTS aggregated_nonces (
     txid TEXT NOT NULL,
     input_index INTEGER NOT NULL,
-    sec_nonce BLOB NOT NULL,
+    agg_nonce TEXT NOT NULL,
     PRIMARY KEY (txid, input_index)
 );
 
--- Table to store unique message hashes per (txid, input_index)
-CREATE TABLE collected_messages (
-    txid TEXT NOT NULL,
-    input_index INTEGER NOT NULL,
-    msg_hash BLOB NOT NULL,             -- Hash is the same no matter who signs it
-    PRIMARY KEY (txid, input_index)
-);
-
--- Table to store partial signatures per operator for each (txid, input_index)
-CREATE TABLE collected_signatures (
-    txid TEXT NOT NULL,
-    input_index INTEGER NOT NULL,
+-- Table to store partial signatures for each operator
+CREATE TABLE IF NOT EXISTS partial_signatures (
     operator_id INTEGER NOT NULL,
-    partial_signature TEXT NOT NULL, -- Signature stored as hex string (different for each operator)
-    FOREIGN KEY (txid, input_index) REFERENCES collected_messages(txid, input_index) ON DELETE CASCADE,
-    PRIMARY KEY (txid, input_index, operator_id)
-);
-
--- Table to store selected outpoints that have been used for KickoffTx
-CREATE TABLE selected_outpoints (
     txid TEXT NOT NULL,
-    vout INTEGER NOT NULL,
-    PRIMARY KEY (txid, vout)
+    input_index INTEGER NOT NULL,
+    partial_signature TEXT NOT NULL,
+    PRIMARY KEY (operator_id, txid, input_index)
 );
 
--- Table to store main KickoffInfo details for each Deposit Txid
-CREATE TABLE kickoff_info (
-    txid TEXT PRIMARY KEY,                -- Unique [deposit] identifier for KickoffInfo
-    change_address TEXT NOT NULL,         -- Change address as a string
-    change_address_network TEXT NOT NULL, -- Network associated with the address (e.g., "bitcoin", "testnet")
-    change_amount INTEGER NOT NULL        -- Change amount in smallest denomination (i.e., satoshis)
-);
-
--- Table to store funding inputs for each KickoffInfo
-CREATE TABLE funding_inputs (
-    kickoff_txid TEXT NOT NULL,           -- Foreign key to kickoff_info.txid
-    input_txid TEXT NOT NULL,             -- Txid of the funding input
-    vout INTEGER NOT NULL,                -- Output index in the funding input
-    FOREIGN KEY (kickoff_txid) REFERENCES kickoff_info(txid) ON DELETE CASCADE,
-    PRIMARY KEY (kickoff_txid, input_txid, vout)
-);
-
--- Table to store funding UTXOs for each KickoffInfo, storing TxOut fields directly
-CREATE TABLE funding_utxos (
-    kickoff_txid TEXT NOT NULL,           -- Foreign key to kickoff_info.txid
-    value INTEGER NOT NULL,               -- Value of the TxOut in smallest denomination (e.g., satoshis)
-    script_pubkey TEXT NOT NULL,          -- Serialized ScriptPubKey in hex format
-    FOREIGN KEY (kickoff_txid) REFERENCES kickoff_info(txid) ON DELETE CASCADE
+-- Table to store witnesses for each operator
+CREATE TABLE IF NOT EXISTS witnesses (
+    operator_id INTEGER NOT NULL,
+    txid TEXT NOT NULL,
+    input_index INTEGER NOT NULL,
+    witness TEXT NOT NULL,
+    PRIMARY KEY (operator_id, txid, input_index)
 );
 
 -- Table to store duty status information with JSON serialization for the status
