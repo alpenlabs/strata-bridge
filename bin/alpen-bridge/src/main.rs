0e721c1df53683e6b75e8612f31393aa2cf90eba
Azz <azz@alpenlabs.io>
2025-05-30 17:57:05 +0100

    pprof generation

--- bin/alpen-bridge/src/main.rs
+++ bin/alpen-bridge/src/main.rs
@@ -18,6 +18,14 @@ mod rpc_server;
 
 mod constants;
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[allow(non_upper_case_globals)]
+#[export_name = "malloc_conf"]
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+
 fn main() {
     logging::init(LoggerConfig::with_base_name("bridge-node"));
 
@@ -44,6 +52,40 @@ fn main() {
                 .expect("must be able to create runtime");
 
             runtime.block_on(async move {
+                tokio::spawn(async move {
+                    use axum::{http::StatusCode, response::IntoResponse};
+
+                    async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)> {
+                        let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
+                        require_profiling_activated(&prof_ctl)?;
+                        let pprof = prof_ctl
+                            .dump_pprof()
+                            .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
+                        Ok(pprof)
+                    }
+
+                    /// Checks whether jemalloc profiling is activated an returns an error response
+                    /// if not.
+                    fn require_profiling_activated(
+                        prof_ctl: &jemalloc_pprof::JemallocProfCtl,
+                    ) -> Result<(), (StatusCode, String)> {
+                        if prof_ctl.activated() {
+                            Ok(())
+                        } else {
+                            Err((
+                                axum::http::StatusCode::FORBIDDEN,
+                                "heap profiling not activated".into(),
+                            ))
+                        }
+                    }
+
+                    let app = axum::Router::new()
+                        .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
+
+                    // run our app with hyper, listening globally on port 3000
+                    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+                    axum::serve(listener, app).await.unwrap();
+                });
                 operator::bootstrap(params, config)
                     .await
                     .unwrap_or_else(|e| {
