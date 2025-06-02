8fd40f2f11f7a5deeb1af7ae4fa1ab3934baa06a
Rajil Bajracharya <imrb1213@gmail.com>
2025-06-02 22:50:35 +0545

    feat(dev-cli): add command to publish challenge tx

--- bin/dev-cli/src/cli.rs
+++ bin/dev-cli/src/cli.rs
@@ -1,3 +1,4 @@
+use bitcoin::Txid;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -16,6 +17,8 @@ pub(crate) enum Commands {
     BridgeIn(BridgeInArgs),
 
     BridgeOut(BridgeOutArgs),
+
+    Challenge(ChallengeArgs),
 }
 
 #[derive(Parser, Debug, Clone)]
@@ -60,4 +63,47 @@ pub(crate) struct BridgeOutArgs {
 
     #[arg(long, help = "the private key for an address in strata")]
     pub(crate) private_key: String,
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(about = "Send challenge transaction", version)]
+pub(crate) struct ChallengeArgs {
+    #[arg(
+        long,
+        env = "CLAIM_TXID",
+        value_parser = clap::value_parser!(Txid),
+        help = "the txid of the claim being challenged"
+    )]
+    pub(crate) claim_txid: Txid,
+
+    #[arg(
+        long,
+        help = "url of the bitcoind node",
+        env = "BTC_URL",
+        default_value = "http://localhost:18443/wallet/default"
+    )]
+    pub(crate) btc_url: String,
+
+    #[arg(
+        long,
+        help = "user for the bitcoind node",
+        env = "BTC_USER",
+        default_value = "rpcuser"
+    )]
+    pub(crate) btc_user: String,
+
+    #[arg(
+        long,
+        help = "password for the bitcoind node",
+        env = "BTC_PASS",
+        default_value = "rpcpassword"
+    )]
+    pub(crate) btc_pass: String,
+
+    #[arg(
+        long,
+        env = "BRIDGE_NODE_URL",
+        help = "the url of the bridge node to query for challenge signature"
+    )]
+    pub(crate) bridge_node_url: String,
 }
