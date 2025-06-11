5ad9fb4b2fe9a05372a8741570ecc70e0c9a33e0
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-06-11 15:14:40 -0600

    algebra: add canonical predicate combinators

---
+++ crates/algebra/src/predicate.rs
@@ -0,0 +1,37 @@
+//! This module provides function definitions for all of the canonical predicate combinators.
+use crate::boolean;
+
+/// Predicate combinator for the ! operation.
+pub fn not<A>(f: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| !f(a)
+}
+
+/// Predicate combinator for the && operation.
+pub fn and<A>(f: impl Fn(&A) -> bool, g: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| f(a) && g(a)
+}
+
+/// Predicate combinator for the || operation.
+pub fn or<A>(f: impl Fn(&A) -> bool, g: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| f(a) || g(a)
+}
+
+/// Predicate combinator for the xor operation.
+pub fn xor<A>(f: impl Fn(&A) -> bool, g: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| f(a) ^ g(a)
+}
+
+/// Predicate combinator for the nand operation.
+pub fn nand<A>(f: impl Fn(&A) -> bool, g: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| boolean::nand(f(a), g(a))
+}
+
+/// Predicate combinator for the nor operation.
+pub fn nor<A>(f: impl Fn(&A) -> bool, g: impl Fn(&A) -> bool) -> impl Fn(&A) -> bool {
+    move |a| boolean::nor(f(a), g(a))
+}
+
+/// Contravariant functor map over predicates.
+pub fn contramap<A, B>(f: impl Fn(&A) -> B, p: impl Fn(&B) -> bool) -> impl Fn(&A) -> bool {
+    move |a| p(&f(a))
+}
