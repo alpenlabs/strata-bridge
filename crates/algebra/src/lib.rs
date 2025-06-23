a4b3400eab111f049a986ae71ace5d57c27b9159
Keagan McClelland <keagan.mcclelland@gmail.com>
2025-06-23 13:25:18 -0600

    algebra: enable compiler features for advanced binder specification

--- crates/algebra/src/lib.rs
+++ crates/algebra/src/lib.rs
@@ -1,6 +1,7 @@
 //! Algebraic Abstractions that relies heavily on the Functional Programming paradigm.
 
 #![feature(coverage_attribute)]
+#![feature(closure_lifetime_binder)]
 
 pub mod boolean;
 pub mod category;
