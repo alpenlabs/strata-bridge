8b26504cba39192db8893630284878c427691cfc
Jose Storopoli <jose@storopoli.com>
2025-06-27 08:34:21 -0300

    chore(deps): bump strata-p2p

--- bin/alpen-bridge/src/main.rs
+++ bin/alpen-bridge/src/main.rs
@@ -1,4 +1,9 @@
 //! The Alpen Bridge is a bridge node for the Alpen BitVM rollup.
+#![allow(
+    incomplete_features,
+    reason = "`strata-p2p` needs `generic_const_exprs` which itself is an `incomplete_feature`"
+)]
+#![feature(generic_const_exprs)] //strata-p2p
 
 use std::{fs, path::Path, thread::sleep};
 
