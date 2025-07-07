60c742bf1b632a68c7069d95b88a9a7575a8ed01
Bibek Pandey <bewakepandey@gmail.com>
2025-07-07 18:15:52 +0545

    Str 1474 mock checkpoints (#202)
    * (bin/mock-checkpoint): Initial cli
    
    * (bin/mock-checkpoint): Update deposit entries, add some modules
    
    * Add readme
    
    * Rename EnvelopeParams to EnvelopeConfig
    
    * Update cargo dependency references
    
    * Cleanups
    
    * fix(mock-checkpoint): load and parse seq xpriv
    
    * Update deposit entries parsing
    
    * Final cleanup
    
    * tests(mock-checkpoint): add test data for deposit in Dispatched state
    
    * chore(mock-checkpoint): add sample deposit entries
    
    * refactor(mock-checkpoint): use consistent arg names and defaults, add `From` impl
    
    * chore(mock-checkpoint): re-export `tracing` from `common` crate and replace `println` calls
    
    * chore: add btcio crate to workspace and use it for mock-checkpoint
    
    ---------
    
    Co-authored-by: Bibek Pandey <bibek@alpenlabs.com>
    Co-authored-by: Rajil Bajracharya <imrb1213@gmail.com>

---
+++ bin/mock-checkpoint/src/chainstate.rs
@@ -0,0 +1,49 @@
+use arbitrary::{Arbitrary, Unstructured};
+use strata_bridge_common::tracing::info;
+use strata_state::{bridge_state::DepositEntry, chain_state::Chainstate};
+
+/// Chainstate wrapper which ensures the chainstate always has empty deposit table.
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct ChainstateWithEmptyDeposits(Chainstate);
+
+impl ChainstateWithEmptyDeposits {
+    /// Creates raw arbitrary chainstate.
+    pub(crate) fn new() -> Self {
+        let mut raw = Unstructured::new(&[]);
+        let chst: Chainstate = Arbitrary::arbitrary(&mut raw).unwrap();
+        // Make sure deposits_table is empty
+        assert!(
+            chst.deposits_table().is_empty(),
+            "Chainstate deposits table is not empty"
+        );
+        Self(chst)
+    }
+
+    pub(crate) fn into_inner(self) -> Chainstate {
+        self.0
+    }
+}
+
+/// Updates deposit entries of given chainstate that had empty deposits.
+pub(crate) fn update_deposit_entries(
+    chainstate: ChainstateWithEmptyDeposits,
+    dep_entries: &[DepositEntry],
+) -> Chainstate {
+    let mut chs = chainstate.into_inner();
+    let dep_table = chs.deposits_table_mut();
+
+    info!("updating deposit entries in chainstate");
+    for entry in dep_entries {
+        // Can only create Accepted deposit entry.
+        let idx = dep_table.create_next_deposit(
+            *entry.output(),
+            entry.notary_operators().to_vec(),
+            entry.amt(),
+        );
+        // Now update the state and withdrawal txid
+        let dep_entry = dep_table.get_deposit_mut(idx).unwrap();
+        dep_entry.set_state(entry.deposit_state().clone());
+        dep_entry.set_withdrawal_request_txid(entry.withdrawal_request_txid());
+    }
+    chs
+}
