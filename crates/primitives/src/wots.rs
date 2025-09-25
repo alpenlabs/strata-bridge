dfdfdc4820e0264533019ecfd1ca76c78bb8c914
abishek <abishek@alpenlabs.io>
2025-09-25 15:00:48 +0545

    fix(wots): resolve memory safety issues in   Arbitrary implementations

--- crates/primitives/src/wots.rs
+++ crates/primitives/src/wots.rs
@@ -151,9 +151,14 @@ impl Arbitrary for Wots256PublicKey {
     type Parameters = ();
 
     fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
-        any::<[u8; std::mem::size_of::<Wots256PublicKey>()]>()
+        any::<[u8; std::mem::size_of::<<wots256 as Wots>::PublicKey>()]>()
             .no_shrink()
-            .prop_map(|arr| unsafe { std::mem::transmute(arr) })
+            .prop_map(|arr| {
+                #[allow(clippy::missing_transmute_annotations)]
+                Wots256PublicKey(Arc::new(unsafe {
+                    std::mem::transmute::<_, <wots256 as Wots>::PublicKey>(arr)
+                }))
+            })
             .boxed()
     }
 
@@ -370,9 +375,14 @@ impl Arbitrary for Groth16PublicKeys {
     type Parameters = ();
 
     fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
-        any::<[u8; std::mem::size_of::<Groth16PublicKeys>()]>()
+        any::<[u8; std::mem::size_of::<BitVmG16PublicKeys>()]>()
             .no_shrink()
-            .prop_map(|arr| unsafe { std::mem::transmute(arr) })
+            .prop_map(|arr| {
+                Groth16PublicKeys(Arc::new(unsafe {
+                    #[allow(clippy::missing_transmute_annotations)]
+                    std::mem::transmute::<_, BitVmG16PublicKeys>(arr)
+                }))
+            })
             .boxed()
     }
 
