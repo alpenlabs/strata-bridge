fa8d24c5741ea5d3d57b9ac6a0f9e5d5cdf67bd0
Christian Lewe <ch_lewe@agdsn.me>
2025-12-04 14:55:25 -0500

    fix: disable unused features

--- crates/tx-graph2/src/lib.rs
+++ crates/tx-graph2/src/lib.rs
@@ -1,11 +1,4 @@
 //! This crate enables the creation and verification of a Glock transaction graph.
 
-#![feature(duration_constructors)] // for constructing `Duration::from_days`
-#![allow(incomplete_features)]
-#![feature(generic_const_exprs)] // this feature is used in size computations
-#![feature(array_try_from_fn)] // this feature is used to generate arrays in a fallible way
-#![feature(maybe_uninit_array_assume_init)] // this feature is used to implement deserialization for fixed length arrays bound to the assert
-                                            // vector length
-
 pub mod connectors;
 pub mod transactions;
