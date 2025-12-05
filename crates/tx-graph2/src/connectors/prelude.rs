d3492fcdb828062bda5443ca0cde4f1a9203e1db
Christian Lewe <ch_lewe@agdsn.me>
2025-12-05 15:16:13 -0500

    feat: CPFP connector

--- crates/tx-graph2/src/connectors/prelude.rs
+++ crates/tx-graph2/src/connectors/prelude.rs
@@ -1,3 +1,5 @@
 //! This module exports all connectors in this crate for convenience.
 
-pub use super::{claim_contest_connector::*, claim_payout_connector::*, n_of_n_connector::*};
+pub use super::{
+    claim_contest_connector::*, claim_payout_connector::*, cpfp_connector::*, n_of_n_connector::*,
+};
