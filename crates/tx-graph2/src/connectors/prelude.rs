aa9a8b76031805a35518766523310b8441f3ed78
Christian Lewe <ch_lewe@agdsn.me>
2025-12-05 14:41:49 -0500

    feat: n-of-n connector

--- crates/tx-graph2/src/connectors/prelude.rs
+++ crates/tx-graph2/src/connectors/prelude.rs
@@ -1,3 +1,3 @@
 //! This module exports all connectors in this crate for convenience.
 
-pub use super::{claim_contest_connector::*, claim_payout_connector::*};
+pub use super::{claim_contest_connector::*, claim_payout_connector::*, n_of_n_connector::*};
