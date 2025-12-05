d02d47392dbfc3a4d76be587fa91c28fc93e630a
Christian Lewe <ch_lewe@agdsn.me>
2025-12-05 15:21:16 -0500

    refactor: Use new CPFP connector
    Stop using old connectors crate as a dependency.

--- crates/tx-graph2/src/transactions/claim.rs
+++ crates/tx-graph2/src/transactions/claim.rs
@@ -1,11 +1,10 @@
 //! This module contains the claim transaction.
 
-use bitcoin::{transaction, Amount, OutPoint, Transaction, TxOut};
-use strata_bridge_connectors::prelude::ConnectorCpfp;
+use bitcoin::{transaction, OutPoint, Transaction, TxOut};
 use strata_bridge_primitives::scripts::prelude::{create_tx, create_tx_ins, create_tx_outs};
 
 use crate::connectors::{
-    prelude::{ClaimContestConnector, ClaimPayoutConnector},
+    prelude::{ClaimContestConnector, ClaimPayoutConnector, CpfpConnector},
     Connector,
 };
 
@@ -30,11 +29,12 @@ impl ClaimTx {
         data: ClaimData,
         claim_contest_connector: ClaimContestConnector,
         claim_payout_connector: ClaimPayoutConnector,
-        cpfp_connector: ConnectorCpfp,
+        cpfp_connector: CpfpConnector,
     ) -> Self {
         let claim_funds = data.claim_funds;
         let claim_contest_tx_out = claim_contest_connector.tx_out();
         let claim_payout_tx_out = claim_payout_connector.tx_out();
+        let cpfp_tx_out = cpfp_connector.tx_out();
 
         let tx_ins = create_tx_ins([claim_funds]);
         let scripts_and_amounts = [
@@ -46,7 +46,7 @@ impl ClaimTx {
                 claim_payout_tx_out.script_pubkey,
                 claim_contest_tx_out.value,
             ),
-            (cpfp_connector.locking_script(), Amount::ZERO),
+            (cpfp_tx_out.script_pubkey, cpfp_tx_out.value),
         ];
         let tx_outs = create_tx_outs(scripts_and_amounts);
 
