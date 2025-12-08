efa853ca4b95d6c864b774ac1b50f228ea9e7b82
Christian Lewe <ch_lewe@agdsn.me>
2025-12-08 14:54:12 -0500

    chore: Rename X-connector -> X

--- crates/tx-graph2/src/connectors/prelude.rs
+++ crates/tx-graph2/src/connectors/prelude.rs
@@ -1,6 +1,3 @@
 //! This module exports all connectors in this crate for convenience.
 
-pub use super::{
-    claim_contest_connector::*, claim_payout_connector::*, cpfp_connector::*, n_of_n_connector::*,
-    timelocked_n_of_nr::*,
-};
+pub use super::{claim_contest::*, claim_payout::*, cpfp::*, n_of_n::*, timelocked_n_of_nr::*};
