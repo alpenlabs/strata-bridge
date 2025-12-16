b740f91ea6bf9b31dc62473a8e51cf5b1ca74c53
abishek <abishek@alpenlabs.io>
2025-12-16 15:11:36 +0545

    functest: add bridge network env

---
+++ functional-tests/tests/fn_network_test.py
@@ -0,0 +1,19 @@
+import flexitest
+
+from utils.utils import wait_until_bridge_ready
+
+
+@flexitest.register
+class BridgeRpcTest(flexitest.Test):
+    def __init__(self, ctx: flexitest.InitContext):
+        ctx.set_env("network")
+
+    def main(self, ctx: flexitest.RunContext):
+        for i in range(3):
+            bo = ctx.get_service(f"bo_{i}")
+            borpc = bo.create_rpc()
+            wait_until_bridge_ready(borpc)
+            operators = borpc.stratabridge_bridgeOperators()
+            assert len(operators) == 1
+
+        return True
