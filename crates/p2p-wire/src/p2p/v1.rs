0d7e939fe0d8e52ffe5e0e703ff8f9446e4c7a6c
Rajil Bajracharya <imrb1213@gmail.com>
2025-12-17 18:04:37 +0545

    Revert "fix: use  lint in p2p-wire"
    This reverts commit 911212d8ec6a3b005177e0ab4f16c5c3d7d78b6a.

--- crates/p2p-wire/src/p2p/v1.rs
+++ crates/p2p-wire/src/p2p/v1.rs
@@ -1,6 +1,5 @@
 //! Strata P2P protocol v1 messages.
 
-#[allow(missing_docs)] // Auto-generated module, docs are not in our control.
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/strata.bitvm2.p2p.v1.rs"));
 }
