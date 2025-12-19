b942ff2b61eb7371b2a40449ea03376a5e358b82
abishek <abishek@alpenlabs.io>
2025-12-19 14:51:13 +0545

    chore: remove the sp1 guest build

--- bridge-guest-builder/src/lib.rs
+++ bridge-guest-builder/src/lib.rs
@@ -1,13 +1,4 @@
-//! Build script for the bridge guest.
+//! Bridge guest builder library.
 
-use sp1_sdk as _;
-#[cfg(not(skip_guest_build))]
-use sp1_sdk::include_elf;
-
-/// The guest bridge ELF.
-#[cfg(skip_guest_build)]
+/// The Mock ELF binary for the bridge guest code
 pub const GUEST_BRIDGE_ELF: &[u8] = &[];
-
-/// The guest bridge ELF.
-#[cfg(not(skip_guest_build))]
-pub const GUEST_BRIDGE_ELF: &[u8] = include_elf!("strata-bridge-guest");
