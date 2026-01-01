55dc523391d3cf6501819e0a0b646ac47928d78e
abishek <abishek@alpenlabs.io>
2026-01-01 17:44:32 +0545

    bridge-sm: fill state for created, graph-gen, deposit-nonce & partials collection

--- crates/bridge-sm/src/deposit/state.rs
+++ crates/bridge-sm/src/deposit/state.rs
@@ -4,7 +4,14 @@
 //! with respect to the multisig. Each state represents a specific point in the process
 //! of handling a deposit, from the initial request to the final spend.
 
-use std::fmt::Display;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fmt::Display,
+};
+
+use bitcoin::{OutPoint, Transaction};
+use musig2::{AggNonce, PartialSignature, PubNonce, secp256k1::schnorr::Signature};
+use strata_bridge_primitives::types::{BitcoinBlockHeight, OperatorIdx};
 
 use crate::{
     deposit::{
@@ -17,16 +24,107 @@ use crate::{
 };
 
 /// The state machine for the Deposit.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum DepositSM {
-    /// TODO: (@MdTeach)
-    Created,
-    /// TODO: (@MdTeach)
-    GraphGenerated,
-    /// TODO: (@MdTeach)
-    DepositNoncesCollected,
-    /// TODO: (@MdTeach)
-    DepositPartialsCollected,
+    /// From the confirmation of the deposit request transaction until all operators
+    /// have generated and linked their graphs for this deposit.
+    Created {
+        /// Index identifying this deposit.
+        deposit_idx: u32,
+
+        /// The unsigned deposit transaction derived from the deposit request.
+        deposit_transaction: Transaction,
+
+        /// Block height where the deposit request transaction was confirmed.
+        drt_block_height: BitcoinBlockHeight,
+
+        /// Outpoint of the deposit request UTXO, used for abort handling.
+        deposit_request_outpoint: OutPoint,
+
+        /// Index of the deposit output in the deposit transaction.
+        output_index: u32,
+
+        /// Latest Bitcoin block height observed by the state machine.
+        block_height: u32,
+
+        /// Operators whose spending graphs have been generated for this deposit.
+        linked_graphs: BTreeSet<OperatorIdx>,
+    },
+    /// From the point where all operator graphs are generated until all public nonces
+    /// required to sign the deposit transaction are collected.
+    GraphGenerated {
+        /// Index identifying this deposit.
+        deposit_idx: u32,
+
+        /// The unsigned deposit transaction to be signed.
+        deposit_transaction: Transaction,
+
+        /// Block height where the deposit request transaction was confirmed.
+        drt_block_height: BitcoinBlockHeight,
+
+        /// Outpoint of the deposit request UTXO.
+        deposit_request_outpoint: OutPoint,
+
+        /// Index of the deposit output in the deposit transaction.
+        output_index: u32,
+
+        /// Latest Bitcoin block height observed by the state machine.
+        block_height: u32,
+
+        /// Public nonces provided by each operator for signing.
+        pubnonces: BTreeMap<OperatorIdx, PubNonce>,
+    },
+    /// From the collection of all deposit public nonces until all partial signatures
+    /// have been received.
+    DepositNoncesCollected {
+        /// Index identifying this deposit.
+        deposit_idx: u32,
+
+        /// The deposit transaction being signed.
+        deposit_transaction: Transaction,
+
+        /// Block height where the deposit request transaction was confirmed.
+        drt_block_height: BitcoinBlockHeight,
+
+        /// Outpoint of the deposit request UTXO.
+        deposit_request_outpoint: OutPoint,
+
+        /// Index of the deposit output in the deposit transaction.
+        output_index: u32,
+
+        /// Latest Bitcoin block height observed by the state machine.
+        block_height: u32,
+
+        /// Aggregated nonce used to validate partial signatures.
+        agg_nonce: AggNonce,
+
+        /// Partial signatures from operators for the deposit transaction.
+        partial_signatures: BTreeMap<OperatorIdx, PartialSignature>,
+    },
+    /// From the collection of all partial signatures until the deposit transaction
+    /// is broadcast and confirmed.
+    DepositPartialsCollected {
+        /// Index identifying this deposit.
+        deposit_idx: u32,
+
+        /// Index of the deposit output in the deposit transaction.
+        output_index: u32,
+
+        /// Latest Bitcoin block height observed by the state machine.
+        block_height: u32,
+
+        /// Block height where the deposit request transaction was confirmed.
+        drt_block_height: BitcoinBlockHeight,
+
+        /// Outpoint of the deposit request UTXO.
+        deposit_request_outpoint: OutPoint,
+
+        /// The fully signed deposit transaction.
+        deposit_transaction: Transaction,
+
+        /// Aggregated signature for the deposit transaction.
+        agg_signature: Signature,
+    },
     /// TODO: (@mukeshdroid)
     Deposited,
     /// TODO: (@mukeshdroid)
@@ -48,10 +146,10 @@ pub enum DepositSM {
 impl Display for DepositSM {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let state_str = match self {
-            DepositSM::Created => "Created",
-            DepositSM::GraphGenerated => "GraphGenerated",
-            DepositSM::DepositNoncesCollected => "DepositNoncesCollected",
-            DepositSM::DepositPartialsCollected => "DepositPartialsCollected",
+            DepositSM::Created { .. } => "Created",
+            DepositSM::GraphGenerated { .. } => "GraphGenerated",
+            DepositSM::DepositNoncesCollected { .. } => "DepositNoncesCollected",
+            DepositSM::DepositPartialsCollected { .. } => "DepositPartialsCollected",
             DepositSM::Deposited => "Deposited",
             DepositSM::Assigned => "Assigned",
             DepositSM::Fulfilled => "Fulfilled",
@@ -62,13 +160,6 @@ impl Display for DepositSM {
             DepositSM::Aborted => "Aborted",
         };
         write!(f, "{}", state_str)
-    }
-}
-
-impl Default for DepositSM {
-    fn default() -> Self {
-        // TODO: (@MdTeach) Remove this impl once `new` starts taking arguments.
-        DepositSM::new()
     }
 }
 
@@ -108,7 +199,8 @@ pub type DSMOutput = SMOutput<DepositDuty, DepositSignal>;
 impl DepositSM {
     /// Creates a new [`DepositSM`] in the `Created` state.
     pub const fn new() -> Self {
-        DepositSM::Created
+        // TODO: (@MdTeach) Add parameters as needed for initialization.
+        todo!()
     }
 
     // **DESIGN PRINCIPLE**
