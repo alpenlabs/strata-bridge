b0e2b90ae844cbf63accac44760d6a1c69670171
abishek <abishek@alpenlabs.io>
2026-01-01 18:59:35 +0545

    bridge-sm: fill publish duties with required fields

--- crates/bridge-sm/src/deposit/duties.rs
+++ crates/bridge-sm/src/deposit/duties.rs
@@ -1,15 +1,36 @@
 //! The duties that need to be performed in the Deposit State Machine in response to the state
 //! transitions.
 
+use bitcoin::{OutPoint, Transaction};
+use musig2::{
+    AggNonce,
+    secp256k1::{Message, schnorr::Signature},
+};
+
 /// The duties that need to be performed to drive the Deposit State Machine forward.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum DepositDuty {
-    /// TODO: (@MdTeach)
-    PublishDepositNonce,
-    /// TODO: (@MdTeach)
-    PublishDepositPartial,
-    /// TODO: (@MdTeach)
-    PublishDeposit,
+    /// publish this operator's nonce for spending the drt
+    PublishDepositNonce {
+        /// DRT outpoint to ID the signing session
+        deposit_out_point: OutPoint,
+    },
+    /// publish this operator's partial signature for spending the drt
+    PublishDepositPartial {
+        /// DRT outpoint to resume the earlier signing session
+        deposit_out_point: OutPoint,
+        /// sighash to be signed for the deposit transaction
+        deposit_sighash: Message,
+        /// aggregated nonce from all operators for this signing session
+        deposit_agg_nonce: AggNonce,
+    },
+    /// publish the deposit transaction to the Bitcoin network
+    PublishDeposit {
+        /// fully constructed deposit transaction
+        deposit_tx: Transaction,
+        /// aggregate signature combining all partial signatures
+        agg_signature: Signature,
+    },
     /// TODO: (@mukeshdroid)
     FulfillWithdrawal,
     /// TODO: (@mukeshdroid)
@@ -27,9 +48,9 @@ pub enum DepositDuty {
 impl std::fmt::Display for DepositDuty {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let duty_str = match self {
-            DepositDuty::PublishDepositNonce => "PublishDepositNonce",
-            DepositDuty::PublishDepositPartial => "PublishDepositPartial",
-            DepositDuty::PublishDeposit => "PublishDeposit",
+            DepositDuty::PublishDepositNonce { .. } => "PublishDepositNonces",
+            DepositDuty::PublishDepositPartial { .. } => "PublishDepositPartials",
+            DepositDuty::PublishDeposit { .. } => "PublishDeposit",
             DepositDuty::FulfillWithdrawal => "FulfillWithdrawal",
             DepositDuty::RequestPayoutNonces => "RequestPayoutNonces",
             DepositDuty::PublishPayoutNonce => "PublishPayoutNonce",
