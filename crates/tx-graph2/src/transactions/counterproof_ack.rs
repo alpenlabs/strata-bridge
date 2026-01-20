f62eb0f7139eda0b68f8db8fae30181fc3158948
Christian Lewe <ch_lewe@agdsn.me>
2026-01-20 14:12:57 -0500

    chore: assert that tx pays zero fees

--- crates/tx-graph2/src/transactions/counterproof_ack.rs
+++ crates/tx-graph2/src/transactions/counterproof_ack.rs
@@ -4,7 +4,7 @@ use bitcoin::{
     absolute,
     sighash::{Prevouts, SighashCache},
     transaction::Version,
-    OutPoint, Psbt, Transaction, TxIn, TxOut, Txid,
+    Amount, OutPoint, Psbt, Transaction, TxIn, TxOut, Txid,
 };
 use secp256k1::schnorr;
 use strata_bridge_connectors2::{
@@ -80,6 +80,14 @@ impl CounterproofAckTx {
             },
         ];
         let output = vec![cpfp_connector.tx_out()];
+
+        let value_in: Amount = prevouts.iter().map(|x| x.value).sum();
+        let value_out: Amount = output.iter().map(|x| x.value).sum();
+        debug_assert!(
+            value_in == value_out,
+            "tx should pay zero fees (value in = {value_in}, value out = {value_out})"
+        );
+
         let tx = Transaction {
             version: Version(3),
             lock_time: absolute::LockTime::ZERO,
--- crates/tx-graph2/src/transactions/slash.rs
+++ crates/tx-graph2/src/transactions/slash.rs
@@ -106,6 +106,13 @@ impl SlashTx {
         }));
         output.push(cpfp_connector.tx_out());
 
+        let value_in: Amount = prevouts.iter().map(|x| x.value).sum();
+        let value_out: Amount = output.iter().map(|x| x.value).sum();
+        debug_assert!(
+            value_in == value_out,
+            "tx should pay zero fees (value in = {value_in}, value out = {value_out})"
+        );
+
         let tx = Transaction {
             version: Version(3),
             lock_time: absolute::LockTime::ZERO,
