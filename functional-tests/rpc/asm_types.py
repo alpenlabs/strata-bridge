0e1f6f7d62000aa9df8e0e2c73978f1675cd2432
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-02-03 17:21:29 +0545

    refactor: improve type safety by using attributes instead of dict

--- functional-tests/rpc/asm_types.py
+++ functional-tests/rpc/asm_types.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
@@ -11,6 +13,10 @@ class L1BlockCommitment:
     height: int
     blkid: str
 
+    @classmethod
+    def from_dict(cls, data: dict) -> L1BlockCommitment:
+        return cls(height=data["height"], blkid=data["blkid"])
+
 
 @dataclass
 class AsmWorkerStatus:
@@ -22,6 +28,17 @@ class AsmWorkerStatus:
     is_initialized: bool
     cur_block: L1BlockCommitment | None
     cur_state: dict | None
+
+    @classmethod
+    def from_dict(cls, data: dict) -> AsmWorkerStatus:
+        cur_block = None
+        if data.get("cur_block") is not None:
+            cur_block = L1BlockCommitment.from_dict(data["cur_block"])
+        return cls(
+            is_initialized=data["is_initialized"],
+            cur_block=cur_block,
+            cur_state=data.get("cur_state"),
+        )
 
 
 @dataclass
