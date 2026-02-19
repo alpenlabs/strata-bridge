6364abe4828c66b6e603c15ace6150526611bd2c
Rajil Bajracharya <imrb1213@gmail.com>
2026-02-19 23:52:05 +0545

    feat(orchestrator): add classifier for p2p and assignment events

--- crates/orchestrator/src/lib.rs
+++ crates/orchestrator/src/lib.rs
@@ -13,6 +13,7 @@
 //! - Driving state machines that implement the core business-logic of the bridge.
 //! - Providing a channel of communication between the various state machines in the bridge.
 
+pub mod events_classifier;
 pub mod events_mux;
 pub mod events_router;
 pub mod sm_registry;
