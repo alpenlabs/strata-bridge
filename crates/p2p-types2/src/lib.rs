255221434336126404ca85d3923a208c205fa543
Rajil Bajracharya <imrb1213@gmail.com>
2026-02-25 21:05:11 +0545

    feat(p2p-types): add types to support graph data exchange

--- crates/p2p-types2/src/lib.rs
+++ crates/p2p-types2/src/lib.rs
@@ -5,11 +5,13 @@
 
 mod bitcoin;
 mod descriptor;
+mod graph_data;
 mod messages;
 mod operator;
 
 pub use bitcoin::{PartialSignature, PubNonce};
 pub use descriptor::PayoutDescriptor;
+pub use graph_data::ClaimInput;
 pub use messages::{GossipsubMsg, MuSig2Nonce, MuSig2Partial, UnsignedGossipsubMsg};
 pub use operator::P2POperatorPubKey;
 pub use strata_bridge_primitives::types::GraphIdx;
