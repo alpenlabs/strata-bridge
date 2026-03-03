846ed0df77d8d3adc4fffb0b72b1f9597ff42473
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-03 17:58:45 +0545

    feat(bin): create scaffolding for new bridge node
    Initial attempt was to just update the existing node but it required a
    massive number of changes just to get the code to compile. So in the
    interest of making reviews easier, a new binary is being created which
    will replace the existing one.

---
+++ bin/strata-bridge/src/constants.rs
@@ -0,0 +1,5 @@
+/// Default thread count for the bridge node.
+pub(crate) const DEFAULT_THREAD_COUNT: u8 = 4;
+
+/// Default thread stack size for the bridge node.
+pub(crate) const DEFAULT_THREAD_STACK_SIZE: usize = 100 * 1024 * 1024;
---
+++ bin/strata-bridge/src/mode/watchtower.rs
@@ -0,0 +1,15 @@
+//! Defines the main loop for the bridge-client in watchtower mode.
+use strata_tasks::TaskExecutor;
+use tracing::info;
+
+use crate::config::Config;
+
+/// Bootstraps the bridge client in watchtower mode by hooking up all the required auxiliary
+/// services including database, rpc server, graceful shutdown handler, etc.
+///
+/// NOTE: (@Rajil1213) this is currently a stub and will be implemented in the future.
+pub(crate) async fn bootstrap(_config: Config, _executor: TaskExecutor) -> anyhow::Result<()> {
+    info!("bootstrapping watchtower node");
+
+    unimplemented!()
+}
