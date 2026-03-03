1e25e56aa0124d248e436778a222c12837eedf22
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-03 19:38:25 +0545

    feat(bin): add top-level params

--- bin/strata-bridge/src/mode/watchtower.rs
+++ bin/strata-bridge/src/mode/watchtower.rs
@@ -2,13 +2,17 @@
 use strata_tasks::TaskExecutor;
 use tracing::info;
 
-use crate::config::Config;
+use crate::{config::Config, params::Params};
 
 /// Bootstraps the bridge client in watchtower mode by hooking up all the required auxiliary
 /// services including database, rpc server, graceful shutdown handler, etc.
 ///
 /// NOTE: (@Rajil1213) this is currently a stub and will be implemented in the future.
-pub(crate) async fn bootstrap(_config: Config, _executor: TaskExecutor) -> anyhow::Result<()> {
+pub(crate) async fn bootstrap(
+    _params: Params,
+    _config: Config,
+    _executor: TaskExecutor,
+) -> anyhow::Result<()> {
     info!("bootstrapping watchtower node");
 
     unimplemented!()
