7c6b16aa4cfa9edb0b2df9ec3f04b37375940f5e
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-03 19:57:15 +0545

    feat(bin): initialize s2

---
+++ bin/strata-bridge/src/mode/services/mod.rs
@@ -0,0 +1,6 @@
+//! Initializers for various external services.
+
+pub(crate) mod btc_client;
+pub(crate) mod operator_table;
+pub(crate) mod operator_wallet;
+pub(in crate::mode) mod secret_service;
