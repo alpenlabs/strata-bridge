282ca9ac342e5213ab87b6399e720dda3da2689c
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-04 17:14:14 +0545

    feat(docker): update docker files to use new binary and config

--- docker/alpen-bridge/entrypoint.sh
+++ docker/alpen-bridge/entrypoint.sh
@@ -4,4 +4,4 @@ CONFIG_FILE="${CONFIG_FILE:-/app/config.toml}"
 
 sleep 5 # wait for bitcoind to be ready
 
-/usr/local/bin/alpen-bridge $MODE --params $PARAMS_FILE --config $CONFIG_FILE
+/usr/local/bin/strata-bridge "$MODE" --params "$PARAMS_FILE" --config "$CONFIG_FILE"
