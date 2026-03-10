c027cbd9d841659f5523901a467035a1288dba91
abishek <abishek@alpenlabs.io>
2026-03-10 19:54:24 +0545

    feat(dev-cli): implement dev-cli create-and-publish-mock-checkpoint

---
+++ bin/dev-cli/src/handlers/checkpoint/mod.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+use crate::cli::CreateAndPublishMockCheckpointArgs;
+
+pub(crate) async fn handle_create_and_publish_mock_checkpoint(
+    _args: CreateAndPublishMockCheckpointArgs,
+) -> Result<()> {
+    todo!()
+}
