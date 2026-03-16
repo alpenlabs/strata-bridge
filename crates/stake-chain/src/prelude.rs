d12f2f9a0683fb3ed0f6d999647955997e26e682
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-16 18:20:41 +0545

    chore: remove stake-chain

--- crates/stake-chain/src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Just import this if you want a no-brainer `use` statement to get the most of the `stake-chain`
-//! crate.
-
-pub use crate::{
-    errors::StakeChainError,
-    stake_chain::StakeChain,
-    transactions::{constants::*, pre_stake::PreStakeTx, stake::StakeTx},
-};
