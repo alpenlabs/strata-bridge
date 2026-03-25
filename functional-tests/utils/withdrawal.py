4d3a83bbca479e486c9eef9bb3d829042c7d8344
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-25 19:26:46 +0545

    feat(fntests): add support for claims RPCs

---
+++ functional-tests/utils/withdrawal.py
@@ -0,0 +1,60 @@
+import logging
+from dataclasses import dataclass
+
+from rpc.types import RpcPendingWithdrawalInfo
+from utils.utils import wait_until
+
+
+@dataclass
+class PendingWithdrawalClaim:
+    """The active claim currently associated with the assigned operator."""
+
+    deposit_idx: int
+    assigned_operator: int
+    claim_txid: str
+
+
+def wait_until_active_valid_claim(
+    bridge_rpc,
+    timeout=300,
+) -> PendingWithdrawalClaim:
+    """Wait until the assigned operator for the only pending withdrawal has an active claim."""
+
+    result: dict[str, PendingWithdrawalClaim | None] = {"active_claim": None}
+
+    def check_pending_withdrawal():
+        pending_withdrawals: list[int] = bridge_rpc.stratabridge_pendingWithdrawals()
+        logging.info(f"Current pending withdrawals: {pending_withdrawals}")
+
+        if len(pending_withdrawals) != 1:
+            return False
+
+        deposit_idx = pending_withdrawals[0]
+        pending_withdrawal_data = bridge_rpc.stratabridge_pendingWithdrawalInfo(deposit_idx)
+        logging.info(f"Pending withdrawal info for {deposit_idx}: {pending_withdrawal_data}")
+
+        if pending_withdrawal_data is None:
+            return False
+
+        pending_withdrawal = RpcPendingWithdrawalInfo.from_json(pending_withdrawal_data)
+        if pending_withdrawal.assigned_claim is None:
+            return False
+
+        result["active_claim"] = PendingWithdrawalClaim(
+            deposit_idx=deposit_idx,
+            assigned_operator=pending_withdrawal.assigned_operator,
+            claim_txid=pending_withdrawal.assigned_claim.claim_txid,
+        )
+        return True
+
+    wait_until(
+        check_pending_withdrawal,
+        timeout=timeout,
+        step=1,
+        error_msg=(
+            f"Timeout after {timeout} seconds waiting for the assigned operator active claim"
+        ),
+    )
+
+    assert result["active_claim"] is not None
+    return result["active_claim"]
