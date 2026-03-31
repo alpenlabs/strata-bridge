63952e2a19364a520b6c5652cb7c66f15e9a044f
Rajil Bajracharya <imrb1213@gmail.com>
2026-03-31 18:30:00 +0545

    feat(bridge-exec): add duties to publish signed txs
    This includes the stake tx, unstaking intent tx and unstaking tx.

--- crates/bridge-exec/src/stake/unstaking.rs
+++ crates/bridge-exec/src/stake/unstaking.rs
@@ -4,24 +4,48 @@
 //! transaction itself.
 
 use bitcoin::{OutPoint, Transaction, secp256k1::schnorr};
+use btc_tracker::event::TxStatus;
+use strata_bridge_connectors::prelude::UnstakingIntentWitness;
 use strata_bridge_tx_graph::transactions::prelude::UnstakingIntentTx;
 
-use crate::{config::ExecutionConfig, errors::ExecutorError, output_handles::OutputHandles};
+use crate::{
+    chain::publish_signed_transaction, errors::ExecutorError, output_handles::OutputHandles,
+    stake::utils::get_preimage,
+};
 
 pub(crate) async fn publish_unstaking_intent(
-    _cfg: &ExecutionConfig,
-    _output_handles: &OutputHandles,
-    _stake_funding_outpoint: OutPoint,
-    _unstaking_intent_tx: UnstakingIntentTx,
-    _n_of_n_signature: &schnorr::Signature,
+    output_handles: &OutputHandles,
+    stake_funds: OutPoint,
+    unstaking_intent_tx: UnstakingIntentTx,
+    n_of_n_signature: &schnorr::Signature,
 ) -> Result<(), ExecutorError> {
-    todo!()
+    let preimage = get_preimage(&output_handles.s2_client, stake_funds).await?;
+
+    let unstaking_intent_witness = UnstakingIntentWitness {
+        n_of_n_signature: *n_of_n_signature,
+        unstaking_preimage: preimage,
+    };
+
+    let signed_unstaking_intent_tx = unstaking_intent_tx.finalize(&unstaking_intent_witness);
+
+    publish_signed_transaction(
+        &output_handles.tx_driver,
+        &signed_unstaking_intent_tx,
+        "unstaking intent tx",
+        TxStatus::is_buried,
+    )
+    .await
 }
 
 pub(crate) async fn publish_unstaking_tx(
-    _cfg: &ExecutionConfig,
-    _output_handles: &OutputHandles,
-    _signed_tx: &Transaction,
+    output_handles: &OutputHandles,
+    signed_tx: &Transaction,
 ) -> Result<(), ExecutorError> {
-    todo!()
+    publish_signed_transaction(
+        &output_handles.tx_driver,
+        signed_tx,
+        "unstaking tx",
+        TxStatus::is_buried,
+    )
+    .await
 }
---
+++ crates/bridge-exec/src/stake/utils.rs
@@ -0,0 +1,22 @@
+//! Common functionalities across different modules in the stake state machine duty execution
+//! context.
+
+use bitcoin::OutPoint;
+use secret_service_client::SecretServiceClient;
+use secret_service_proto::v2::traits::{SecretService, StakeChainPreimages};
+
+use crate::errors::ExecutorError;
+
+pub(super) async fn get_preimage(
+    s2_client: &SecretServiceClient,
+    outpoint: OutPoint,
+) -> Result<[u8; 32], ExecutorError> {
+    const STAKE_TX_INDEX: u32 = 0; // there is only one stake tx.
+
+    let preimage_client = s2_client.stake_chain_preimages();
+
+    preimage_client
+        .get_preimg(outpoint.txid, outpoint.vout, STAKE_TX_INDEX)
+        .await
+        .map_err(ExecutorError::SecretServiceErr)
+}
