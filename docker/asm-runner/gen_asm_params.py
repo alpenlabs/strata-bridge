af8028102db1cc1ae7e3e8303b50ac8aa5fc7101
Rajil Bajracharya <imrb1213@gmail.com>
2026-04-01 16:35:08 +0545

    fix(docker): set `asm-runner` genesis height to be before bridge's

--- docker/asm-runner/gen_asm_params.py
+++ docker/asm-runner/gen_asm_params.py
@@ -133,8 +133,8 @@ def validate_params(asm_params: dict, bridge_params: dict) -> None:
             # TODO: <https://atlassian.alpenlabs.net/browse/STR-2572>
             # set these to be equal after the above bug is fixed
             # for now, just check that the ASM genesis height is 1 less than the Bridge genesis height
-            asm_params["l1_view"]["blk"]["height"] - 1,  # must start sooner
-            bridge_params["genesis_height"],
+            asm_params["l1_view"]["blk"]["height"],  # must start sooner
+            bridge_params["genesis_height"] - 1,
         ),
         ("denomination", asm["denomination"], bridge["deposit_amount"]),
         ("operator_fee", asm["operator_fee"], bridge["operator_fee"]),
