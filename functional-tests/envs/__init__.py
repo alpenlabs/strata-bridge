6d37fdaffc0074c005ee8e4b840cadc07c375de3
Sapin Bajracharya <sapin@alpenlabs.io>
2026-04-03 10:57:36 +0545

    fn-test: use asm env for fn_asm_block_test

--- functional-tests/envs/__init__.py
+++ functional-tests/envs/__init__.py
@@ -1,6 +1,14 @@
 from .asm_config import AsmEnvConfig
+from .asm_env import AsmEnv
 from .basic_env import BasicEnv, StrataLiveEnv
 from .bridge_network_env import BridgeNetworkEnv
 from .btc_config import BitcoinEnvConfig
 
-__all__ = ["AsmEnvConfig", "BasicEnv", "BridgeNetworkEnv", "BitcoinEnvConfig", "StrataLiveEnv"]
+__all__ = [
+    "AsmEnv",
+    "AsmEnvConfig",
+    "BasicEnv",
+    "BridgeNetworkEnv",
+    "BitcoinEnvConfig",
+    "StrataLiveEnv",
+]
