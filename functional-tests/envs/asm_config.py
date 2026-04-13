89ed9744326c8333ab52f5ce8a6c3dfafb57674e
abishek <abishek@alpenlabs.io>
2026-04-13 08:24:04 +0545

    test(func-tests): add assignment duration timeout reassignment test

--- functional-tests/envs/asm_config.py
+++ functional-tests/envs/asm_config.py
@@ -10,5 +10,5 @@ class AsmEnvConfig:
     magic: str = ASM_MAGIC_BYTES
     denomination: int = 1_000_000_000
     assignment_duration: int = 10_000
-    operator_fee: int = 100_000_000
+    operator_fee: int = 10_000_000
     recovery_delay: int = 1_008
